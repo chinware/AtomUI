d5eef378da4b457954db685c69e1d48717742b93
polarboy <chinware@163.com>
2024-07-22 15:59:03 +0800

    Menu 样式定制完成

--- src/AtomUI.Controls/Menu/Menu.cs
+++ src/AtomUI.Controls/Menu/Menu.cs
@@ -1,10 +1,22 @@
-﻿using AtomUI.Styling;
+﻿using Avalonia;
 
 namespace AtomUI.Controls;
 
 using AvaloniaMenu = Avalonia.Controls.Menu;
 
-public class Menu : AvaloniaMenu, IControlCustomStyle
+public class Menu : AvaloniaMenu,
+                    ISizeTypeAware
 {
-   
+   #region 公共属性定义
+
+   public static readonly StyledProperty<SizeType> SizeTypeProperty =
+      AvaloniaProperty.Register<Button, SizeType>(nameof(SizeType), SizeType.Middle);
+
+   public SizeType SizeType
+   {
+      get => GetValue(SizeTypeProperty);
+      set => SetValue(SizeTypeProperty, value);
+   }
+
+   #endregion
 }
--- src/AtomUI.Controls/Menu/MenuTheme.cs
+++ src/AtomUI.Controls/Menu/MenuTheme.cs
@@ -1,9 +1,11 @@
 ﻿using AtomUI.Styling;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Templates;
 using Avalonia.Input;
 using Avalonia.Layout;
+using Avalonia.Styling;
 
 namespace AtomUI.Controls;
 
@@ -33,7 +35,36 @@ public class MenuTheme : ControlTheme
             HorizontalAlignment = HorizontalAlignment.Stretch,
             Child = itemPresenter,
          };
+         CreateTemplateParentBinding(border, Border.PaddingProperty, Menu.PaddingProperty);
+         CreateTemplateParentBinding(border, Border.BackgroundProperty, Menu.BackgroundProperty);
+         CreateTemplateParentBinding(border, Border.BackgroundSizingProperty, Menu.BackgroundSizingProperty);
+         CreateTemplateParentBinding(border, Border.BorderThicknessProperty, Menu.BorderThicknessProperty);
+         CreateTemplateParentBinding(border, Border.BorderBrushProperty, Menu.BorderBrushProperty);
+         CreateTemplateParentBinding(border, Border.CornerRadiusProperty, Menu.CornerRadiusProperty);
          return border;
       });
+   }
+
+   protected override void BuildStyles()
+   {
+      var commonStyle = new Style(selector => selector.Nesting());
+      commonStyle.Add(Menu.BackgroundProperty, GlobalResourceKey.ColorBgContainer);
+      commonStyle.Add(Menu.PaddingProperty, new Thickness(0));
+      commonStyle.Add(Menu.BorderBrushProperty, GlobalResourceKey.ColorBorder);
+      var largeSizeType = new Style(selector => selector.Nesting().PropertyEquals(Menu.SizeTypeProperty, SizeType.Large));
+      largeSizeType.Add(Menu.MinHeightProperty, GlobalResourceKey.ControlHeightLG);
+      largeSizeType.Add(Menu.CornerRadiusProperty, GlobalResourceKey.BorderRadius);
+      commonStyle.Add(largeSizeType);
+      
+      var middleSizeType = new Style(selector => selector.Nesting().PropertyEquals(Menu.SizeTypeProperty, SizeType.Middle));
+      middleSizeType.Add(Menu.MinHeightProperty, GlobalResourceKey.ControlHeight);
+      middleSizeType.Add(Menu.CornerRadiusProperty, GlobalResourceKey.BorderRadius);
+      commonStyle.Add(middleSizeType);
+      
+      var smallSizeType = new Style(selector => selector.Nesting().PropertyEquals(Menu.SizeTypeProperty, SizeType.Small));
+      smallSizeType.Add(Menu.MinHeightProperty, GlobalResourceKey.ControlHeightSM);
+      smallSizeType.Add(Menu.CornerRadiusProperty, GlobalResourceKey.BorderRadiusSM);
+      commonStyle.Add(smallSizeType);
+      Add(commonStyle);
    }
 }
