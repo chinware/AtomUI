fe7ea95236ef583ed166efbfb66ecd4acf4ab0b9
polarboy <chinware@163.com>
2024-08-03 13:17:07 +0800

    实现 BaseTabControl
    完成基础的 BaseTabControl 类，支持两个风格

--- src/AtomUI.Controls/TabControl/BaseTabControl.cs
+++ src/AtomUI.Controls/TabControl/BaseTabControl.cs
@@ -1,6 +1,126 @@
-﻿namespace AtomUI.Controls;
+﻿using AtomUI.Data;
+using AtomUI.Theme.Data;
+using AtomUI.Theme.Styling;
+using AtomUI.Utils;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
 
-public class BaseTabControl
+namespace AtomUI.Controls;
+
+using AvaloniaTabControl = Avalonia.Controls.TabControl;
+
+public class BaseTabControl : AvaloniaTabControl
 {
+   public const string TopPC    = ":top";
+   public const string RightPC  = ":right";
+   public const string BottomPC = ":bottom";
+   public const string LeftPC   = ":left";
    
+   private static readonly FuncTemplate<Panel?> DefaultPanel =
+      new(() => new StackPanel());
+   
+   #region 公共属性定义
+   public static readonly StyledProperty<SizeType> SizeTypeProperty =
+      AvaloniaProperty.Register<BaseTabControl, SizeType>(nameof(SizeType), SizeType.Middle);
+
+   public static readonly StyledProperty<bool> TabAlignmentCenterProperty =
+      AvaloniaProperty.Register<BaseTabControl, bool>(nameof(TabAlignmentCenter), false);
+   
+   public SizeType SizeType
+   {
+      get => GetValue(SizeTypeProperty);
+      set => SetValue(SizeTypeProperty, value);
+   }
+   
+   public bool TabAlignmentCenter
+   {
+      get => GetValue(TabAlignmentCenterProperty);
+      set => SetValue(TabAlignmentCenterProperty, value);
+   }
+   
+   #endregion
+   
+    private Border? _frameDecorator;
+   
+   static BaseTabControl()
+   {
+      ItemsPanelProperty.OverrideDefaultValue<BaseTabControl>(DefaultPanel);
+   }
+   
+   protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+   {
+      base.OnApplyTemplate(e);
+      _frameDecorator = e.NameScope.Find<Border>(BaseTabControlTheme.FrameDecoratorPart);
+      SetupBorderBinding();
+   }
+
+   private void SetupBorderBinding()
+   {
+      if (_frameDecorator is not null) {
+         TokenResourceBinder.CreateTokenBinding(this, BorderThicknessProperty, GlobalResourceKey.BorderThickness, BindingPriority.Template,
+                                                new RenderScaleAwareThicknessConfigure(this));
+      }
+   }
+
+   protected override void PrepareContainerForItemOverride(Control container, object? item, int index)
+   {
+      base.PrepareContainerForItemOverride(container, item, index);
+      if (container is TabItem tabItem) {
+         BindUtils.RelayBind(this, SizeTypeProperty, tabItem, TabItem.SizeTypeProperty);
+      }
+   }
+
+   protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+   {
+      base.OnAttachedToLogicalTree(e);
+      UpdatePseudoClasses();
+   }
+
+   protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+   {
+      base.OnPropertyChanged(change);
+      if (change.Property == TabStripPlacementProperty) {
+         UpdatePseudoClasses();
+      }
+   }
+
+   private void UpdatePseudoClasses()
+   {
+      PseudoClasses.Set(TopPC, TabStripPlacement == Dock.Top);
+      PseudoClasses.Set(RightPC, TabStripPlacement == Dock.Right);
+      PseudoClasses.Set(BottomPC, TabStripPlacement == Dock.Bottom);
+      PseudoClasses.Set(LeftPC, TabStripPlacement == Dock.Left);
+   }
+
+   public override void Render(DrawingContext context)
+   {
+      Point startPoint = default;
+      Point endPoint = default;
+      var borderThickness = BorderThickness.Left;
+      var offsetDelta = borderThickness / 2;
+      if (TabStripPlacement == Dock.Top) {
+         startPoint = new Point(0, Bounds.Height - offsetDelta);
+         endPoint = new Point(Bounds.Width, Bounds.Height - offsetDelta);
+      } else if (TabStripPlacement == Dock.Right) {
+         startPoint = new Point(offsetDelta, 0);
+         endPoint = new Point(offsetDelta, Bounds.Height);
+      } else if (TabStripPlacement == Dock.Bottom) {
+         startPoint = new Point(0, offsetDelta);
+         endPoint = new Point(Bounds.Width, offsetDelta);
+      } else {
+         startPoint = new Point(Bounds.Width - offsetDelta, 0);
+         endPoint = new Point(Bounds.Width - offsetDelta, Bounds.Height);
+      }
+
+      using var optionState = context.PushRenderOptions(new RenderOptions()
+      {
+         EdgeMode = EdgeMode.Aliased
+      });
+      context.DrawLine(new Pen(BorderBrush, borderThickness), startPoint, endPoint);
+   }
 }
