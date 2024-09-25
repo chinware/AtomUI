c9c4d71e6a0eb6259f36907fc391c2ce3bfca0ea
zeroone <zeroone@chinware.com>
2024-09-25 15:30:47 +0800

    Update build configuration.

--- build/!Package.All.Release.bat
+++ build/!Package.All.Release.bat
@@ -1,9 +1,13 @@
 %~dp0
 
-rd/s/q ..\_output\
+rd/s/q ../_output/
 
-cd ..\
+cd ../
 
-dotnet msbuild .\AtomUI.sln /p:Configuration=Release
+dotnet msbuild ./AtomUI.sln /p:Configuration=Release
+
+cd packages/AtomUI/
+
+dotnet msbuild ./AtomUI.csproj /p:Configuration=Release
 
 pause
