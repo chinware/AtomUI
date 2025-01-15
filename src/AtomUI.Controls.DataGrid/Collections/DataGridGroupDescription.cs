571eeab076ece40dcc6cba592a29f36adb048c16
chinboy <chinboy@qinware.com>
2025-01-15 16:14:45 +0800

    Add DataGridPathGroupDescription

---
+++ src/AtomUI.Controls.DataGrid/Collections/DataGridGroupDescription.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.Globalization;
+using Avalonia.Collections;
+
+namespace AtomUI.Controls.Collections;
+
+public abstract class DataGridGroupDescription : INotifyPropertyChanged
+{
+    public AvaloniaList<object> GroupKeys { get; }
+    
+    public DataGridGroupDescription()
+    {
+        GroupKeys = new AvaloniaList<object>();
+        GroupKeys.CollectionChanged += (sender, e) => OnPropertyChanged(new PropertyChangedEventArgs(nameof(GroupKeys)));
+    }
+    
+    protected virtual event PropertyChangedEventHandler? PropertyChanged;
+    
+    event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
+    {
+        add => PropertyChanged += value;
+        remove => PropertyChanged -= value;
+    }
+    
+    protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        PropertyChanged?.Invoke(this, e);
+    }
+
+    public virtual string PropertyName => String.Empty;
+    public abstract object GroupKeyFromItem(object item, int level, CultureInfo culture);
+    public virtual bool KeysMatch(object groupKey, object itemKey)
+    {
+        return object.Equals(groupKey, itemKey);
+    }
+}
