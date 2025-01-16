345eb99871d31e5939525d5d6eae953486c55e2e
chinboy <chinboy@qinware.com>
2025-01-16 08:57:36 +0800

    Add DataGridEnumerations enums

---
+++ src/AtomUI.Controls.DataGrid/DataGridLength.cs
@@ -0,0 +1,15 @@
+namespace AtomUI.Controls;
+
+public enum DataGridLengthUnitType
+{
+    Auto = 0,
+    Pixel = 1,
+    SizeToCells = 2,
+    SizeToHeader = 3,
+    Star = 4
+}
+
+public class DataGridLength
+{
+    
+}
