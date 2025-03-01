eeaa54c4d55b37857bac5bcea80136f5828c6d83
chinboy <chinboy@qinware.com>
2025-03-01 18:02:01 +0800

    Add AnimationAwareControlProperty

--- src/AtomUI.Theme/IAnimationAwareControl.cs
+++ src/AtomUI.Theme/IAnimationAwareControl.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace AtomUI.Controls;
@@ -6,8 +7,19 @@ public interface IAnimationAwareControl
 {
     public const string IsMotionEnabledPropertyName = "IsMotionEnabled";
     public const string IsWaveAnimationEnabledPropertyName = "IsWaveAnimationEnabled";
-    
+
     bool IsMotionEnabled { get; }
     bool IsWaveAnimationEnabled { get; }
     Control PropertyBindTarget { get; }
+}
+
+public abstract class AnimationAwareControlProperty : AvaloniaObject
+{
+    public static readonly StyledProperty<bool> IsMotionEnabledProperty
+        = AvaloniaProperty.Register<AnimationAwareControlProperty, bool>(IAnimationAwareControl
+            .IsMotionEnabledPropertyName);
+
+    public static readonly StyledProperty<bool> IsWaveAnimationEnabledProperty
+        = AvaloniaProperty.Register<AnimationAwareControlProperty, bool>(IAnimationAwareControl
+            .IsWaveAnimationEnabledPropertyName);
 }
