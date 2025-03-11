d9df1856263e364b8bed4486858a396d27591a0c
CHINBOY <qinware@163.com>
2025-03-11 19:01:43 +0800

    Refactor Generator config

--- scripts/BuildAtomUINative.ps1
+++ scripts/BuildAtomUINative.ps1
@@ -29,8 +29,8 @@ try {
         return
     }
 } catch [System.IO.IOException] {
-    if (Test-Path $path) {
-        Write-Host "目录已被其他进程创建: $path"
+    if (Test-Path $buildMarker) {
+        Write-Host "目录已被其他进程创建: $buildMarker"
     } else {
         Write-Host "创建失败: $_，跳过编译"
     }
--- scripts/PublishToLocalSources.ps1
+++ scripts/PublishToLocalSources.ps1
@@ -3,8 +3,12 @@ param (
     [string]$buildType = "Release"
 )
 
-dotnet build -v diag --configuration $buildType ../AtomUI.sln
-dotnet pack --no-build --configuration $buildType /p:IsNightlyBuild=true ../packages/AtomUI/AtomUI.csproj
+$buildTimestamp = Get-Date -Format 'yyyyMMddHHmm'
+
+dotnet build -v diag --configuration $buildType ../packages/AtomUI/AtomUI.csproj
+dotnet pack --no-build --configuration $buildType /p:IsNightlyBuild=true /p:BuildTimestamp=$buildTimestamp ../packages/AtomUI/AtomUI.csproj
+dotnet pack --no-build --configuration $buildType /p:IsNightlyBuild=true /p:BuildTimestamp=$buildTimestamp ../src/AtomUI.IconPkg.Generator/AtomUI.IconPkg.Generator.csproj
+dotnet pack --no-build --configuration $buildType /p:IsNightlyBuild=true /p:BuildTimestamp=$buildTimestamp ../src/AtomUI.Generator/AtomUI.Generator.csproj
 
 # 探测包
 $packages = Get-ChildItem -Path ../output/Nuget -Filter *.nupkg -Recurse -File
