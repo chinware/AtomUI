d529bd932937100648187132d9a5aeb29935c7f8
CHINBOY <qinware@163.com>
2025-05-25 14:39:35 +0800

    rebase datagrid on develop

---
+++ src/AtomUI.Controls/GeneratedFiles/AtomUI.Generator/AtomUI.Generator.LanguageGenerator/LanguageResourceConst.g.cs
@@ -0,0 +1,48 @@
+﻿using AtomUI.Theme;
+
+namespace AtomUI.Controls.DatePickerLang
+{
+    public static class DatePickerLangResourceKey
+    {
+        public static readonly LanguageResourceKey Now = new LanguageResourceKey("DatePicker.Now", "AtomUI.Lang");
+        public static readonly LanguageResourceKey Today = new LanguageResourceKey("DatePicker.Today", "AtomUI.Lang");
+    }
+}
+
+namespace AtomUI.Controls.Localization
+{
+    public static class CommonLangResourceKey
+    {
+        public static readonly LanguageResourceKey CancelText = new LanguageResourceKey("Common.CancelText", "AtomUI.Lang");
+        public static readonly LanguageResourceKey OkText = new LanguageResourceKey("Common.OkText", "AtomUI.Lang");
+    }
+}
+
+namespace AtomUI.Controls.PaginationLang
+{
+    public static class PaginationLangResourceKey
+    {
+        public static readonly LanguageResourceKey JumpToText = new LanguageResourceKey("Pagination.JumpToText", "AtomUI.Lang");
+        public static readonly LanguageResourceKey PageText = new LanguageResourceKey("Pagination.PageText", "AtomUI.Lang");
+        public static readonly LanguageResourceKey TotalInfoFormat = new LanguageResourceKey("Pagination.TotalInfoFormat", "AtomUI.Lang");
+    }
+}
+
+namespace AtomUI.Controls.PopupConfirmLang
+{
+    public static class PopupConfirmLangResourceKey
+    {
+        public static readonly LanguageResourceKey CancelText = new LanguageResourceKey("PopupConfirm.CancelText", "AtomUI.Lang");
+        public static readonly LanguageResourceKey OkText = new LanguageResourceKey("PopupConfirm.OkText", "AtomUI.Lang");
+    }
+}
+
+namespace AtomUI.Controls.TimePickerLang
+{
+    public static class TimePickerLangResourceKey
+    {
+        public static readonly LanguageResourceKey AMText = new LanguageResourceKey("TimePicker.AMText", "AtomUI.Lang");
+        public static readonly LanguageResourceKey Now = new LanguageResourceKey("TimePicker.Now", "AtomUI.Lang");
+        public static readonly LanguageResourceKey PMText = new LanguageResourceKey("TimePicker.PMText", "AtomUI.Lang");
+    }
+}
---
+++ src/AtomUI.Controls/GeneratedFiles/AtomUI.Generator/AtomUI.Generator.TokenResourceKeyGenerator/ControlTokenRegister.g.cs
@@ -0,0 +1,53 @@
+﻿namespace AtomUI.Theme
+{
+    internal class ControlTokenTypeRegister
+    {
+        internal static void Register()
+        {
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.AddOnDecoratedBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.AlertToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ArrowDecoratedBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.BadgeToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ButtonSpinnerToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ButtonToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.CalendarToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.CaptionButtonToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.CheckBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.CollapseToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ComboBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.DatePickerToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.DrawerToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.EmptyIndicatorToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ExpanderToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.FlyoutPresenterToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.GroupBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.Internal.InfoPickerInputToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.LineEditToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ListBoxToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.LoadingIndicatorToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.MarqueeLabelToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.MenuToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.MessageToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.NavMenuToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.NotificationToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.NumericUpDownToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.OptionButtonToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.PaginationToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.PopupConfirmToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ProgressBarToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.RadioButtonToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ScrollBarToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.SegmentedToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.SeparatorToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.SliderToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.TabControlToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.TagToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.TimelineToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.TimePickerToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ToggleSwitchToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.ToolTipToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.TreeViewToken));
+            ThemeManager.Current.RegisterControlTokenType(typeof(AtomUI.Controls.WindowToken));
+        }
+    }
+}
