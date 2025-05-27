c96d7f13a55557505571d1229d03ea68519eaac6
CHINBOY <qinware@163.com>
2025-05-27 09:10:13 +0800

    Add TypeHelper

---
+++ src/AtomUI.Controls.XDataGrid/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("https://atomui.net", "atom")]
+[assembly: XmlnsDefinition("https://atomui.net", "AtomUI.Controls")]
+[assembly: XmlnsDefinition("https://atomui.net", "AtomUI.Controls.Utils")]
+[assembly: XmlnsDefinition("https://atomui.net", "AtomUI.Controls.XDataGrid")]
