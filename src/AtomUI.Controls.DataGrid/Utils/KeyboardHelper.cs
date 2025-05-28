1ae48b6aa456370da45565cf3c5c618e050376c9
CHINBOY <qinware@163.com>
2025-05-28 11:54:25 +0800

    Add TreeHelper

---
+++ src/AtomUI.Controls.DataGrid/Utils/KeyboardHelper.cs
@@ -0,0 +1,31 @@
+// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace AtomUI.Controls.Utils;
+
+internal static class KeyboardHelper
+{
+    public static void GetMetaKeyState(Control target, KeyModifiers modifiers, out bool ctrlOrCmd, out bool shift)
+    {
+        ctrlOrCmd = modifiers.HasFlag(GetPlatformCtrlOrCmdKeyModifier(target));
+        shift     = modifiers.HasFlag(KeyModifiers.Shift);
+    }
+
+    public static void GetMetaKeyState(Control target, KeyModifiers modifiers, out bool ctrlOrCmd, out bool shift, out bool alt)
+    {
+        ctrlOrCmd = modifiers.HasFlag(GetPlatformCtrlOrCmdKeyModifier(target));
+        shift     = modifiers.HasFlag(KeyModifiers.Shift);
+        alt       = modifiers.HasFlag(KeyModifiers.Alt);
+    }
+
+    public static KeyModifiers GetPlatformCtrlOrCmdKeyModifier(Control? target)
+    {
+        var keymap = TopLevel.GetTopLevel(target)!.PlatformSettings!.HotkeyConfiguration;
+        return keymap.CommandModifiers;
+    }
+}
