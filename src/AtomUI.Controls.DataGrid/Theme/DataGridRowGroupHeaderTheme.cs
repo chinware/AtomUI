f94d657105f6c1a32c83813bcc71491095b31024
CHINBOY <qinware@163.com>
2025-05-28 16:00:28 +0800

    Add DataGridRowGroupHeader

--- src/AtomUI.Controls.DataGrid/Theme/DataGridRowGroupHeaderTheme.cs
+++ src/AtomUI.Controls.DataGrid/Theme/DataGridRowGroupHeaderTheme.cs
@@ -6,6 +6,11 @@ namespace AtomUI.Controls;
 [ControlThemeProvider]
 internal class DataGridRowGroupHeaderTheme : BaseControlTheme
 {
+    public const string ExpanderButtonPart = "PART_ExpanderButton";
+    public const string IndentSpacerPart = "PART_IndentSpacer";
+    public const string ItemCountElementPart = "PART_ItemCountElement";
+    public const string PropertyNameElementPart = "PART_PropertyNameElement";
+    
     public DataGridRowGroupHeaderTheme()
         : base(typeof(DataGridRowGroupHeader))
     {
