b4a37ec4edf33acccfc817b1b5136c5ab7946772
CHINBOY <qinware@163.com>
2025-07-04 11:03:56 +0800

    Add EmbeddableControlRootTheme

---
+++ src/AtomUI.Controls/Embedding/Themes/EmbeddingThemeConstants.cs
@@ -0,0 +1,7 @@
+namespace AtomUI.Controls.Themes;
+
+internal static class EmbeddingThemeConstants
+{
+    public const string TransparencyFallbackPart = "PART_TransparencyFallback";
+    public const string ContentPresenterPart = "PART_ContentPresenter";
+}
