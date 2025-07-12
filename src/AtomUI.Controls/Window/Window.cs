dcf4ada8959f65035fdc4a3a6699b830e52e51dc
CHINBOY <qinware@163.com>
2025-07-12 23:15:08 +0800

    Fix window fullscreen restore issue

--- src/AtomUI.Controls/Window/Window.cs
+++ src/AtomUI.Controls/Window/Window.cs
@@ -1,3 +1,6 @@
+using Avalonia;
+using Avalonia.Controls;
+
 namespace AtomUI.Controls;
 
 using AvaloniaWindow = Avalonia.Controls.Window;
@@ -5,4 +8,25 @@ using AvaloniaWindow = Avalonia.Controls.Window;
 public class Window : AvaloniaWindow
 {
     protected override Type StyleKeyOverride { get; } = typeof(Window);
+
+    private WindowState? _originState;
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == WindowStateProperty)
+        {
+            if (change.NewValue is WindowState newState)
+            {
+                if (newState == WindowState.FullScreen)
+                {
+                    _originState = change.GetOldValue<WindowState>();
+                }
+                else
+                {
+                    WindowState = _originState.HasValue ? _originState.Value : newState;;
+                }
+            }
+        }
+    }
 }
