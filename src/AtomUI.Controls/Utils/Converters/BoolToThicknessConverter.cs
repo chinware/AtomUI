ae3b9b5d7ade34f33978aed03be9389eb800daf7
elarity <elarity@gmail.com>
2025-08-13 09:42:13 +0800

    Remove Console.WriteLine

--- src/AtomUI.Controls/Utils/Converters/BoolToThicknessConverter.cs
+++ src/AtomUI.Controls/Utils/Converters/BoolToThicknessConverter.cs
@@ -10,7 +10,6 @@ internal class BoolToThicknessConverter : IValueConverter
     // 当绑定值为 true 时，用 Parameter 指定的 Thickness，否则返回 Empty
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        Console.WriteLine("Text with Icon, need a padding left.");
         // 没传参数则默认为 4 像素左侧间距
         return new Thickness(4, 0, 0, 0);
     }
