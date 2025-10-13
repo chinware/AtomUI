4915112c2ece19e94009b61492c9090f82d1ea69
CHINBOY <qinware@163.com>
2025-10-13 15:28:57 +0800

    Fixed system font interference issue Added a new default Chinese font

--- src/AtomUI.Theme/TokenSystem/TokenDefinitions/DesignToken.cs
+++ src/AtomUI.Theme/TokenSystem/TokenDefinitions/DesignToken.cs
@@ -33,7 +33,7 @@ public partial class DesignToken : AbstractDesignToken
         ColorWarning     = Color.Parse("#faad14");
         ColorError       = Color.Parse("#ff4d4f");
         ColorInfo        = Color.Parse("#1677ff");
-        FontFamily       = FontFamily.Parse("fonts:AlibabaSans#Alibaba Sans, $Default");
+        FontFamily       = FontFamily.Parse("fonts:AlibabaSans#Alibaba Sans, Segoe UI, Segoe UI Symbol, Helvetica Neue, Noto Sans, Noto Sans CJK SC, 文泉驿正黑, Microsoft YaHei, PingFang SC, $Default");
         BorderRadius     = new CornerRadius(6);
         ColorTransparent = Colors.Transparent;
     }
