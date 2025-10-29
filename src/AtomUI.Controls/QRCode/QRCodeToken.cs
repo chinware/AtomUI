0081ec8959f443698c9418c816613d68ef48c97d
yaobiao131 <28655758+yaobiao131@users.noreply.github.com>
2025-10-29 21:03:42 +0800

    feat: 实现QRCode

---
+++ src/AtomUI.Controls/QRCode/QRCodeToken.cs
@@ -0,0 +1,31 @@
+﻿using AtomUI.Theme.TokenSystem;
+using Avalonia;
+using Avalonia.Media;
+
+namespace AtomUI.Controls;
+
+[ControlDesignToken]
+internal class QRCodeToken : AbstractControlDesignToken
+{
+    public const string ID = "QRCode";
+
+    /// <summary>
+    /// 按钮内间距
+    /// </summary>
+    public Thickness Border { get; set; }
+
+    public Color QRCodeMaskBackgroundColor { get; set; }
+
+    public QRCodeToken()
+        : base(ID)
+    {
+    }
+
+    public override void CalculateFromAlias()
+    {
+        base.CalculateFromAlias();
+        Border = new Thickness(SharedToken.LineWidth);
+        var colorBgContainer = SharedToken.ColorBgContainer;
+        QRCodeMaskBackgroundColor = Color.FromArgb(244, colorBgContainer.R, colorBgContainer.G, colorBgContainer.B);
+    }
+}
