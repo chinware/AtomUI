ea42a64ab2059ee003d6c08d5448df65a0755b78
chinboy <qinware@163.com>
2025-11-20 10:21:26 +0800

    Refactor IconPresenter

--- src/AtomUI.Desktop.Controls/ThemeManagerBuilderExtensions.cs
+++ src/AtomUI.Desktop.Controls/ThemeManagerBuilderExtensions.cs
@@ -1,13 +1,9 @@
 using AtomUI.Controls;
-using AtomUI.Data;
-using AtomUI.Icons.AntDesign;
 using AtomUI.MotionScene;
 using AtomUI.Theme;
 using AtomUI.Theme.Language;
-using AtomUI.Theme.Styling;
 using Avalonia;
 using Avalonia.Animation;
-using Avalonia.Media;
 using Avalonia.Media.Transformation;
 
 namespace AtomUI.Desktop.Controls;
@@ -39,23 +35,5 @@ public static class ThemeManagerBuilderExtensions
     {
         Animation.RegisterCustomAnimator<TransformOperations, MotionTransformOptionsAnimator>();
         AvaloniaLocator.CurrentMutable.BindToSelf(new ToolTipService());
-        var colorTextTertiary = TokenResourceUtils.FindGlobalTokenResource(SharedTokenKey.ColorTextTertiary);
-        if (colorTextTertiary is IBrush defaultFilledColor)
-        {
-            IconProvider<AntDesignIconKind>.DefaultFilledColor = defaultFilledColor;
-        }
-
-        var colorInfoText = TokenResourceUtils.FindGlobalTokenResource(SharedTokenKey.ColorTextTertiary);
-        var colorInfoBg   = TokenResourceUtils.FindGlobalTokenResource(SharedTokenKey.ColorInfoBg);
-
-        if (colorInfoText is IBrush primaryFilledColor)
-        {
-            IconProvider<AntDesignIconKind>.DefaultPrimaryFilledColor = primaryFilledColor;
-        }
-
-        if (colorInfoBg is IBrush secondaryFilledColor)
-        {
-            IconProvider<AntDesignIconKind>.DefaultSecondaryFilledColor = secondaryFilledColor;
-        }
     }
 }
