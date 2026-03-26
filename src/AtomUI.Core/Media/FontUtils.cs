227aca5b06fc9969486502ebfd4453a0ff81ba28
halinhtvn3a <77691576+halinhtvn3a@users.noreply.github.com>
2026-03-27 00:48:23 +0700

    docs(AtomUI.Core): fontutils.convertemtopixel has incorrect calculation formula
    The method incorrectly multiplies by `value` twice: `fontSize * value * renderScaling` then `* value` again.
    This results in pixels = fontSize * value² * renderScaling instead of the correct fontSize * value * renderScaling.
    This is a mathematical bug that will cause incorrect font size calculations throughout the library.
    
    
    Affected files: FontUtils.cs
    
    Signed-off-by: halinhtvn3a <77691576+halinhtvn3a@users.noreply.github.com>

--- src/AtomUI.Core/Media/FontUtils.cs
+++ src/AtomUI.Core/Media/FontUtils.cs
@@ -5,13 +5,12 @@ public static class FontUtils
    /// <summary>
    /// 将 value 的值转换为像素
    /// </summary>
-   /// <param name="value"></param>
-   /// <param name="fontSize"></param>
-   /// <param name="renderScaling"></param>
-   /// <returns></returns>
+   /// <param name="value">EM 单位值</param>
+   /// <param name="fontSize">基础字体大小（像素）</param>
+   /// <param name="renderScaling">渲染缩放比例，默认为 1.0</param>
+   /// <returns>转换后的像素值</returns>
    public static double ConvertEmToPixel(double value, double fontSize, double renderScaling = 1.0)
     {
-        var fontSizePx = fontSize * value * renderScaling;
-        return fontSizePx * value;
+        return fontSize * value * renderScaling;
     }
 }
