03ec864f22ef70a5a8ac2417d87288478c755920
chinboy <qinware@163.com>
2026-04-29 17:02:11 +0800

    refactor(Collapse): migrate Collapse module to Avalonia 12

---
+++ src/AtomUI.Desktop.Controls/Collapse/Collapse.cs
@@ -0,0 +1,410 @@
+﻿using AtomUI.Controls;
+using AtomUI.Desktop.Controls.Themes;
+using AtomUI.Theme;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.VisualTree;
+
+namespace AtomUI.Desktop.Controls;
+
+public enum CollapseTriggerType
+{
+    Header,
+    Icon
+}
+
+public enum CollapseExpandIconPosition
+{
+    Start,
+    End
+}
+
+[TemplatePart(CollapseThemeConstants.ItemsPresenterPart, typeof(ItemsPresenter))]
+public class Collapse : SelectingItemsControl, IMotionAwareControl
+{
+    #region 公共属性定义
+
+    public static readonly StyledProperty<SizeType> SizeTypeProperty =
+        SizeTypeControlProperty.SizeTypeProperty.AddOwner<Collapse>();
+
+    public static readonly StyledProperty<bool> IsGhostStyleProperty =
+        AvaloniaProperty.Register<Collapse, bool>(nameof(IsGhostStyle));
+
+    public static readonly StyledProperty<bool> IsBorderlessProperty =
+        AvaloniaProperty.Register<Collapse, bool>(nameof(IsBorderless));
+
+    public static readonly StyledProperty<bool> IsAccordionProperty =
+        AvaloniaProperty.Register<Collapse, bool>(nameof(IsAccordion));
+
+    public static readonly StyledProperty<CollapseTriggerType> TriggerTypeProperty =
+        AvaloniaProperty.Register<Collapse, CollapseTriggerType>(nameof(TriggerType));
+
+    public static readonly StyledProperty<CollapseExpandIconPosition> ExpandIconPositionProperty =
+        AvaloniaProperty.Register<Collapse, CollapseExpandIconPosition>(nameof(ExpandIconPosition));
+
+    public static readonly StyledProperty<bool> IsMotionEnabledProperty =
+        MotionAwareControlProperty.IsMotionEnabledProperty.AddOwner<Collapse>();
+    
+    public static readonly StyledProperty<Thickness?> ItemHeaderPaddingProperty =
+        AvaloniaProperty.Register<Collapse, Thickness?>(nameof(ItemHeaderPadding));
+    
+    public static readonly StyledProperty<Thickness?> ItemContentPaddingProperty =
+        AvaloniaProperty.Register<Collapse, Thickness?>(nameof(ItemContentPadding));
+
+    public SizeType SizeType
+    {
+        get => GetValue(SizeTypeProperty);
+        set => SetValue(SizeTypeProperty, value);
+    }
+
+    public bool IsGhostStyle
+    {
+        get => GetValue(IsGhostStyleProperty);
+        set => SetValue(IsGhostStyleProperty, value);
+    }
+
+    public bool IsBorderless
+    {
+        get => GetValue(IsBorderlessProperty);
+        set => SetValue(IsBorderlessProperty, value);
+    }
+
+    public bool IsAccordion
+    {
+        get => GetValue(IsAccordionProperty);
+        set => SetValue(IsAccordionProperty, value);
+    }
+
+    public CollapseTriggerType TriggerType
+    {
+        get => GetValue(TriggerTypeProperty);
+        set => SetValue(TriggerTypeProperty, value);
+    }
+
+    public CollapseExpandIconPosition ExpandIconPosition
+    {
+        get => GetValue(ExpandIconPositionProperty);
+        set => SetValue(ExpandIconPositionProperty, value);
+    }
+
+    public bool IsMotionEnabled
+    {
+        get => GetValue(IsMotionEnabledProperty);
+        set => SetValue(IsMotionEnabledProperty, value);
+    }
+    
+    public Thickness? ItemHeaderPadding
+    {
+        get => GetValue(ItemHeaderPaddingProperty);
+        set => SetValue(ItemHeaderPaddingProperty, value);
+    }
+
+    public Thickness? ItemContentPadding
+    {
+        get => GetValue(ItemContentPaddingProperty);
+        set => SetValue(ItemContentPaddingProperty, value);
+    }
+
+    #endregion
+
+    #region 内部属性定义
+
+    internal static readonly DirectProperty<Collapse, Thickness> EffectiveBorderThicknessProperty =
+        AvaloniaProperty.RegisterDirect<Collapse, Thickness>(nameof(EffectiveBorderThickness),
+            o => o.EffectiveBorderThickness,
+            (o, v) => o.EffectiveBorderThickness = v);
+
+    private Thickness _effectiveBorderThickness;
+
+    internal Thickness EffectiveBorderThickness
+    {
+        get => _effectiveBorderThickness;
+        set => SetAndRaise(EffectiveBorderThicknessProperty, ref _effectiveBorderThickness, value);
+    }
+
+    private static readonly FuncTemplate<Panel?> DefaultPanel =
+        new(() => new StackPanel
+        {
+            Orientation = Orientation.Vertical
+        });
+
+    #endregion
+
+    static Collapse()
+    {
+        SelectionModeProperty.OverrideDefaultValue<Collapse>(SelectionMode.Multiple | SelectionMode.Toggle);
+        ItemsPanelProperty.OverrideDefaultValue<Collapse>(DefaultPanel);
+        AutoScrollToSelectedItemProperty.OverrideDefaultValue<Collapse>(false);
+        AffectsRender<Collapse>(SelectionModeProperty);
+    }
+
+    public Collapse()
+    {
+        SelectionChanged += HandleSelectionChanged;
+        this.RegisterTokenResourceScope(CollapseToken.ScopeProvider);
+    }
+
+    private void HandleSelectionChanged(object? sender, SelectionChangedEventArgs args)
+    {
+        SetupItemsBorderThickness();
+    }
+
+    private void SetupItemsBorderThickness()
+    {
+        if (this.IsAttachedToVisualTree())
+        {
+            for (var i = 0; i < ItemCount; ++i)
+            {
+                if (Items[i] is CollapseItem collapseItem)
+                {
+                    SetupCollapseBorderThickness(collapseItem, i);
+                }
+            }
+        }
+    }
+    
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new CollapseItem();
+    }
+
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
+    {
+        return NeedsContainer<CollapseItem>(item, out recycleKey);
+    }
+
+    protected sealed override void PrepareContainerForItemOverride(Control container, object? item, int index)
+    {
+        if (container is CollapseItem collapseItem)
+        {
+            if (item != null && item is not Visual)
+            {
+                if (!collapseItem.IsSet(CollapseItem.ContentProperty))
+                {
+                    collapseItem.SetCurrentValue(CollapseItem.ContentProperty, item);
+                }
+
+                if (item is ICollapseItemData itemData)
+                {
+                    if (!collapseItem.IsSet(CollapseItem.HeaderProperty))
+                    {
+                        collapseItem.SetCurrentValue(CollapseItem.HeaderProperty, itemData.Header);
+                    }
+                    if (!collapseItem.IsSet(CollapseItem.IsSelectedProperty))
+                    {
+                        collapseItem.SetCurrentValue(CollapseItem.IsSelectedProperty, itemData.IsSelected);
+                    }
+                    if (!collapseItem.IsSet(CollapseItem.IsShowExpandIconProperty))
+                    {
+                        collapseItem.SetCurrentValue(CollapseItem.IsShowExpandIconProperty, itemData.IsShowExpandIcon);
+                    }
+                }
+            }
+
+            if (ItemTemplate != null)
+            {
+                collapseItem[!CollapseItem.ContentTemplateProperty] = this[!ItemTemplateProperty];
+            }
+            
+            collapseItem[!CollapseItem.SizeTypeProperty]           = this[!SizeTypeProperty];
+            collapseItem[!CollapseItem.BorderThicknessProperty]    = this[!EffectiveBorderThicknessProperty];
+            collapseItem[!CollapseItem.IsGhostStyleProperty]       = this[!IsGhostStyleProperty];
+            collapseItem[!CollapseItem.IsBorderlessProperty]       = this[!IsBorderlessProperty];
+            collapseItem[!CollapseItem.TriggerTypeProperty]        = this[!TriggerTypeProperty];
+            collapseItem[!CollapseItem.ExpandIconPositionProperty] = this[!ExpandIconPositionProperty];
+            collapseItem[!CollapseItem.IsMotionEnabledProperty]    = this[!IsMotionEnabledProperty];
+            PrepareCollapseItem(collapseItem, item, index);
+            SetupCollapseBorderThickness(collapseItem, index);
+            ConfigureItemPaddings(collapseItem);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(container), "The container type is incorrect, it must be type CollapseItem.");
+        }
+    }
+
+    protected virtual void PrepareCollapseItem(CollapseItem collapseItem, object? item, int index)
+    {
+    }
+
+    protected override void ContainerIndexChangedOverride(Control container, int oldIndex, int newIndex)
+    {
+        if (container is CollapseItem collapseItem)
+        {
+            SetupCollapseBorderThickness(collapseItem, newIndex);
+        }
+    }
+
+    private void SetupCollapseBorderThickness(CollapseItem collapseItem, int index)
+    {
+        var headerBorderBottom  = BorderThickness.Bottom;
+        var contentBorderBottom = BorderThickness.Bottom;
+        if (!IsGhostStyle)
+        {
+            if (!IsBorderless)
+            {
+                if (index == ItemCount - 1 && !collapseItem.IsSelected)
+                {
+                    headerBorderBottom = 0d;
+                }
+            }
+            else
+            {
+                if (collapseItem.IsSelected || (index == ItemCount - 1 && !collapseItem.IsSelected))
+                {
+                    headerBorderBottom = 0d;
+                }
+            }
+
+            if (index == ItemCount - 1 &&
+                (collapseItem.IsSelected || (!collapseItem.IsSelected && collapseItem.InAnimating)))
+            {
+                contentBorderBottom = 0d;
+            }
+        }
+        else
+        {
+            headerBorderBottom  = 0d;
+            contentBorderBottom = 0d;
+        }
+
+        collapseItem.HeaderBorderThickness  = new Thickness(0, 0, 0, headerBorderBottom);
+        collapseItem.ContentBorderThickness = new Thickness(0, 0, 0, contentBorderBottom);
+    }
+
+    protected override void OnGotFocus(FocusChangedEventArgs e)
+    {
+        base.OnGotFocus(e);
+
+        if (e.NavigationMethod == NavigationMethod.Directional)
+        {
+            var containerFromEventSource = GetContainerFromEventSource(e.Source);
+            if (containerFromEventSource is CollapseItem collapseItem)
+            {
+                if (!collapseItem.InAnimating)
+                {
+                    e.Handled = UpdateSelectionFromEvent(collapseItem, e);
+                }
+            }
+        }
+    }
+
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        base.OnPointerPressed(e);
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed && e.Pointer.Type == PointerType.Mouse)
+        {
+            var containerFromEventSource = GetContainerFromEventSource(e.Source);
+            if (containerFromEventSource is CollapseItem collapseItem)
+            {
+                if (!collapseItem.InAnimating && collapseItem.IsPointInHeaderBounds(e.GetPosition(collapseItem)))
+                {
+                    e.Handled = UpdateSelectionFromEvent(collapseItem, e);
+                }
+            }
+        }
+    }
+
+    protected override void OnPointerReleased(PointerReleasedEventArgs e)
+    {
+        if (e.InitialPressMouseButton == MouseButton.Left && e.Pointer.Type != PointerType.Mouse)
+        {
+            var container = GetContainerFromEventSource(e.Source);
+            if (container != null
+                && container.GetVisualsAt(e.GetPosition(container))
+                            .Any(c => container == c || container.IsVisualAncestorOf(c)))
+            {
+                var containerFromEventSource = GetContainerFromEventSource(e.Source);
+                if (containerFromEventSource is CollapseItem collapseItem)
+                {
+                    if (!collapseItem.InAnimating && collapseItem.IsPointInHeaderBounds(e.GetPosition(collapseItem)))
+                    {
+                        e.Handled = UpdateSelectionFromEvent(collapseItem, e);
+                    }
+                }
+            }
+        }
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == IsBorderlessProperty)
+        {
+            SetupEffectiveBorderThickness();
+        }
+        else if (change.Property == IsAccordionProperty)
+        {
+            SetupSelectionMode();
+        }
+        else if (change.Property == IsBorderlessProperty ||
+                 change.Property == IsGhostStyleProperty)
+        {
+            SetupItemsBorderThickness();
+        }
+        if (change.Property == ItemHeaderPaddingProperty ||
+            change.Property == ItemContentPaddingProperty)
+        {
+            if (Items.Count > 0)
+            {
+                for (int i = 0; i < ItemCount; i++)
+                {
+                    var item = Items[i];
+                    if (item is CollapseItem collapseItem)
+                    {
+                        ConfigureItemPaddings(collapseItem);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SetupEffectiveBorderThickness()
+    {
+        if (IsBorderless || IsGhostStyle)
+        {
+            EffectiveBorderThickness = default;
+        }
+        else
+        {
+            EffectiveBorderThickness = BorderThickness;
+        }
+    }
+
+    private void SetupSelectionMode()
+    {
+        if (IsAccordion)
+        {
+            SelectionMode = SelectionMode.Single | SelectionMode.Toggle;
+        }
+        else
+        {
+            SelectionMode = SelectionMode.Multiple | SelectionMode.Toggle;
+        }
+    }
+    
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        SetupEffectiveBorderThickness();
+    }
+    
+    private void ConfigureItemPaddings(CollapseItem collapseItem)
+    {
+        if (collapseItem.HeaderPadding == null)
+        {
+            collapseItem.SetCurrentValue(CollapseItem.HeaderPaddingProperty, ItemHeaderPadding);
+        }
+
+        if (collapseItem.ContentPadding == null)
+        {
+            collapseItem.SetCurrentValue(CollapseItem.ContentPaddingProperty, ItemContentPadding);
+        }
+    }
+}
---
+++ src/AtomUI.Desktop.Controls/Collapse/CollapseToken.cs
@@ -0,0 +1,77 @@
+﻿using AtomUI.Desktop.Controls.DesignTokens;
+using AtomUI.Theme;
+using AtomUI.Theme.TokenSystem;
+using Avalonia;
+using Avalonia.Media;
+
+namespace AtomUI.Desktop.Controls;
+
+[ControlDesignToken]
+internal class CollapseToken : AbstractControlDesignToken
+{
+    public const string ID = "Collapse";
+    public static readonly ControlTokenResourceScopeProvider ScopeProvider = new(ID);
+    
+    public CollapseToken()
+        : base(ID)
+    {
+    }
+
+    /// <summary>
+    /// 折叠面板头部内边距
+    /// </summary>
+    public Thickness HeaderPadding { get; set; }
+
+    /// <summary>
+    /// 折叠面板头部背景
+    /// </summary>
+    public Color HeaderBg { get; set; }
+
+    /// <summary>
+    /// 折叠面板内容内边距
+    /// </summary>
+    public Thickness ContentPadding { get; set; }
+
+    /// <summary>
+    /// 折叠面板内容背景
+    /// </summary>
+    public Color ContentBg { get; set; }
+
+    #region 内部 Token 定义
+
+    public Thickness CollapseHeaderPaddingSM { get; set; }
+    public Thickness CollapseHeaderPaddingLG { get; set; }
+    public Thickness CollapseContentPaddingSM { get; set; }
+    public Thickness CollapseContentPaddingLG { get; set; }
+    public CornerRadius CollapsePanelBorderRadius { get; set; }
+    public Thickness LeftExpandButtonMarginSM { get; set; }
+    public Thickness LeftExpandButtonMargin { get; set; }
+    public Thickness LeftExpandButtonMarginLG { get; set; }
+    public Thickness RightExpandButtonMarginSM { get; set; }
+    public Thickness RightExpandButtonMargin { get; set; }
+    public Thickness RightExpandButtonMarginLG { get; set; }
+
+    #endregion
+
+    public override void CalculateTokenValues(bool isDarkMode)
+    {
+        base.CalculateTokenValues(isDarkMode);
+        HeaderPadding             = new Thickness(SharedToken.UniformlyPadding, SharedToken.UniformlyPaddingSM);
+        HeaderBg                  = SharedToken.ColorFillAlter;
+        ContentPadding            = new Thickness(16, SharedToken.UniformlyPadding);
+        CollapseContentPaddingSM  = SharedToken.PaddingSM;
+        CollapseContentPaddingLG  = SharedToken.PaddingLG;
+        ContentBg                 = SharedToken.ColorBgContainer;
+        CollapseHeaderPaddingSM   = new Thickness(SharedToken.UniformlyPaddingSM, SharedToken.UniformlyPaddingXS);
+        CollapseHeaderPaddingLG   = new Thickness(SharedToken.UniformlyPaddingLG, SharedToken.UniformlyPadding);
+        CollapsePanelBorderRadius = SharedToken.BorderRadiusLG;
+        LeftExpandButtonMarginSM  = new Thickness(0, 0, SharedToken.UniformlyMarginXXS, 0);
+        LeftExpandButtonMargin    = new Thickness(0, 0, SharedToken.UniformlyMarginXS, 0);
+        LeftExpandButtonMarginLG  = new Thickness(0, 0, SharedToken.UniformlyMarginSM, 0);
+        RightExpandButtonMarginSM = new Thickness(SharedToken.UniformlyMarginXXS, 0, 0, 0);
+        RightExpandButtonMargin   = new Thickness(SharedToken.UniformlyMarginXS, 0, 0, 0);
+        RightExpandButtonMarginLG   = new Thickness(SharedToken.UniformlyMarginSM, 0, 0, 0);
+    }
+    
+    protected override Type GetTokenKindType() => typeof(CollapseTokenKind);
+}
