2a9d1243e03f6c301c5b774bc478f5d67b011d3a
chinboy <qinware@163.com>
2026-04-29 17:04:29 +0800

    refactor(Expander): migrate Expander module to Avalonia 12

---
+++ src/AtomUI.Desktop.Controls/Expander/ExpanderPseudoClass.cs
@@ -0,0 +1,13 @@
+namespace AtomUI.Desktop.Controls;
+
+public static class ExpanderPseudoClass
+{
+    public const string Expanded = ":expanded";
+    public const string ExpandUp = ":up";
+    public const string ExpandDown = ":down";
+    public const string ExpandLeft = ":left";
+    public const string ExpandRight = ":right";
+    
+    public const string CustomHeaderPadding = ":custom-header-padding";
+    public const string CustomContentPadding = ":custom-content-padding";
+}
