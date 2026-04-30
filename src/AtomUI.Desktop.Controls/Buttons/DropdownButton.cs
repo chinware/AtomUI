77d5a39f98e6b1dbfcf2d80709bf971069203afc
chinboy <qinware@163.com>
2026-04-30 21:03:29 +0800

    feat(DropdownButton,SplitButton): port DropdownButton and SplitButton from release/5.0 with Avalonia 12 adaptation

---
+++ src/AtomUI.Desktop.Controls/Buttons/DropdownButton.cs
@@ -0,0 +1,267 @@
+﻿using System.Reactive.Disposables;
+using AtomUI.Data;
+using AtomUI.Icons.AntDesign;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Primitives.PopupPositioning;
+using Avalonia.Interactivity;
+
+namespace AtomUI.Desktop.Controls;
+
+using FlyoutControl = Flyout;
+
+public class DropdownButton : Button
+{
+    #region 公共属性定义
+
+    public static readonly StyledProperty<MenuFlyout?> DropdownFlyoutProperty =
+        AvaloniaProperty.Register<DropdownButton, MenuFlyout?>(nameof(DropdownFlyout));
+
+    public static readonly StyledProperty<FlyoutTriggerType> TriggerTypeProperty =
+        FlyoutStateHelper.TriggerTypeProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<bool> IsShowArrowProperty =
+        ArrowDecoratedBox.IsShowArrowProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<bool> IsPointAtCenterProperty =
+        FlyoutControl.IsPointAtCenterProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<PlacementMode> PlacementProperty =
+        Avalonia.Controls.Primitives.Popup.PlacementProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<PopupAnchor> PlacementAnchorProperty =
+        Avalonia.Controls.Primitives.Popup.PlacementAnchorProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<PopupGravity> PlacementGravityProperty =
+        Avalonia.Controls.Primitives.Popup.PlacementGravityProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<double> MarginToAnchorProperty =
+        Popup.MarginToAnchorProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<int> MouseEnterDelayProperty =
+        FlyoutStateHelper.MouseEnterDelayProperty.AddOwner<DropdownButton>();
+
+    public static readonly StyledProperty<int> MouseLeaveDelayProperty =
+        FlyoutStateHelper.MouseLeaveDelayProperty.AddOwner<DropdownButton>();
+    
+    public static readonly StyledProperty<bool> IsShowOpenIndicatorProperty =
+        AvaloniaProperty.Register<DropdownButton, bool>(nameof(IsShowOpenIndicator), true);
+    
+    public static readonly StyledProperty<PathIcon?> OpenIndicatorProperty =
+        AvaloniaProperty.Register<DropdownButton, PathIcon?>(nameof(OpenIndicator));
+
+    public MenuFlyout? DropdownFlyout
+    {
+        get => GetValue(DropdownFlyoutProperty);
+        set => SetValue(DropdownFlyoutProperty, value);
+    }
+
+    public FlyoutTriggerType TriggerType
+    {
+        get => GetValue(TriggerTypeProperty);
+        set => SetValue(TriggerTypeProperty, value);
+    }
+
+    public bool IsShowArrow
+    {
+        get => GetValue(IsShowArrowProperty);
+        set => SetValue(IsShowArrowProperty, value);
+    }
+
+    public bool IsPointAtCenter
+    {
+        get => GetValue(IsPointAtCenterProperty);
+        set => SetValue(IsPointAtCenterProperty, value);
+    }
+
+    public PlacementMode Placement
+    {
+        get => GetValue(PlacementProperty);
+        set => SetValue(PlacementProperty, value);
+    }
+
+    public PopupGravity PlacementGravity
+    {
+        get => GetValue(PlacementGravityProperty);
+        set => SetValue(PlacementGravityProperty, value);
+    }
+
+    public PopupAnchor PlacementAnchor
+    {
+        get => GetValue(PlacementAnchorProperty);
+        set => SetValue(PlacementAnchorProperty, value);
+    }
+
+    public double MarginToAnchor
+    {
+        get => GetValue(MarginToAnchorProperty);
+        set => SetValue(MarginToAnchorProperty, value);
+    }
+
+    public int MouseEnterDelay
+    {
+        get => GetValue(MouseEnterDelayProperty);
+        set => SetValue(MouseEnterDelayProperty, value);
+    }
+
+    public int MouseLeaveDelay
+    {
+        get => GetValue(MouseLeaveDelayProperty);
+        set => SetValue(MouseLeaveDelayProperty, value);
+    }
+    
+    public bool IsShowOpenIndicator
+    {
+        get => GetValue(IsShowOpenIndicatorProperty);
+        set => SetValue(IsShowOpenIndicatorProperty, value);
+    }
+    
+    public PathIcon? OpenIndicator
+    {
+        get => GetValue(OpenIndicatorProperty);
+        set => SetValue(OpenIndicatorProperty, value);
+    }
+
+    #endregion
+
+    #region 公共事件定义
+
+    public static readonly RoutedEvent<FlyoutMenuItemClickedEventArgs> MenuItemClickedEvent =
+        RoutedEvent.Register<DropdownButton, FlyoutMenuItemClickedEventArgs>(
+            nameof(MenuItemClicked),
+            RoutingStrategies.Bubble);
+    
+    public event EventHandler<FlyoutMenuItemClickedEventArgs>? MenuItemClicked
+    {
+        add => AddHandler(MenuItemClickedEvent, value);
+        remove => RemoveHandler(MenuItemClickedEvent, value);
+    }
+    #endregion
+
+    #region 内部属性定义
+
+    internal static readonly DirectProperty<DropdownButton, bool> IsContentVisibleProperty =
+        AvaloniaProperty.RegisterDirect<DropdownButton, bool>(nameof(IsContentVisible),
+            o => o.IsContentVisible,
+            (o, v) => o.IsContentVisible = v);
+        
+    private bool _isContentVisible;
+
+    internal bool IsContentVisible
+    {
+        get => _isContentVisible;
+        set => SetAndRaise(IsContentVisibleProperty, ref _isContentVisible, value);
+    }
+
+    #endregion
+    
+    private readonly FlyoutStateHelper _flyoutStateHelper;
+    private CompositeDisposable? _flyoutBindingDisposables;
+    static DropdownButton()
+    {
+        PlacementProperty.OverrideDefaultValue<DropdownButton>(PlacementMode.BottomEdgeAlignedLeft);
+        IsShowArrowProperty.OverrideDefaultValue<DropdownButton>(false);
+    }
+
+    public DropdownButton()
+    {
+        _flyoutStateHelper = new FlyoutStateHelper
+        {
+            AnchorTarget = this
+        };
+        _flyoutStateHelper[!FlyoutStateHelper.FlyoutProperty]          = this[!DropdownFlyoutProperty];
+        _flyoutStateHelper[!FlyoutStateHelper.MouseEnterDelayProperty] = this[!MouseEnterDelayProperty];
+        _flyoutStateHelper[!FlyoutStateHelper.MouseLeaveDelayProperty] = this[!MouseLeaveDelayProperty];
+        _flyoutStateHelper[!FlyoutStateHelper.TriggerTypeProperty]     = this[!TriggerTypeProperty];
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        if (OpenIndicator == null)
+        {
+            SetCurrentValue(OpenIndicatorProperty, new DownOutlined());
+        }
+    }
+    
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        _flyoutStateHelper.NotifyAttachedToVisualTree();
+        if (DropdownFlyout != null)
+        {
+            SetupFlyoutProperties(DropdownFlyout);
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        _flyoutStateHelper.NotifyDetachedFromVisualTree();
+        _flyoutBindingDisposables?.Dispose();
+        _flyoutBindingDisposables = null;
+    }
+    
+    private void SetupFlyoutProperties(MenuFlyout menuFlyout)
+    {
+        _flyoutBindingDisposables?.Dispose();
+        _flyoutBindingDisposables = new CompositeDisposable(8);
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, PlacementProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, PlacementAnchorProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, PlacementGravityProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, IsShowArrowProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, IsPointAtCenterProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, MarginToAnchorProperty, menuFlyout));
+        _flyoutBindingDisposables.Add(BindUtils.RelayBind(this, IsMotionEnabledProperty, menuFlyout));
+    }
+    
+    private void HandleMenuItemClicked(object? sender, FlyoutMenuItemClickedEventArgs args)
+    {
+        var eventArgs = new FlyoutMenuItemClickedEventArgs(MenuItemClickedEvent, args.Item);
+        RaiseEvent(eventArgs);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == DropdownFlyoutProperty)
+        {
+            if (change.OldValue is MenuFlyout oldMenuFlyout)
+            {
+                _flyoutBindingDisposables?.Dispose();
+                oldMenuFlyout.MenuItemClicked -= HandleMenuItemClicked;
+            }
+
+            if (change.NewValue is MenuFlyout newMenuFlyout)
+            {
+                newMenuFlyout.MenuItemClicked += HandleMenuItemClicked;
+                SetupFlyoutProperties(newMenuFlyout);
+            }
+        }
+
+        if (change.Property == ContentProperty ||
+            change.Property == ContentTemplateProperty ||
+            change.Property == IsLoadingProperty ||
+            change.Property == IconProperty)
+        {
+            ConfigureContentVisible();
+        }
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        if (RightExtraContent == null)
+        {
+            SetCurrentValue(RightExtraContentProperty, new Border());
+        }
+
+        ConfigureContentVisible();
+    }
+
+    private void ConfigureContentVisible()
+    {
+        SetCurrentValue(IsContentVisibleProperty, IsLoading || Content != null || ContentTemplate != null || Icon != null);
+    }
+}
