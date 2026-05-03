96b0953978d6dc66964156be6fcbaad2d408fb65
chinboy <qinware@163.com>
2026-05-03 19:32:41 +0800

    feat(ResultShowCase): add ResultShowCase view and view model for displaying results

--- controlgallery/AtomUIGallery/ShowCases/ViewModels/Feedback/ResultViewModel.cs
+++ controlgallery/AtomUIGallery/ShowCases/ViewModels/Feedback/ResultViewModel.cs
@@ -1,15 +1,14 @@
-using AtomUI.Controls;
 using ReactiveUI;
 
 namespace AtomUIGallery.ShowCases.ViewModels;
 
 public class ResultViewModel : ReactiveObject, IRoutableViewModel
 {
-    public static EntityKey ID = "Result";
+    public const string ID = "Result";
 
     public IScreen HostScreen { get; }
 
-    public string UrlPathSegment { get; } = ID.ToString();
+    public string UrlPathSegment { get; } = ID;
 
     public ResultViewModel(IScreen screen)
     {
