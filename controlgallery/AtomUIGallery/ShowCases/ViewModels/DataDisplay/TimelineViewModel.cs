20375b414f139c4abe8331075e8b30d263da2434
chinboy <qinware@163.com>
2026-05-03 14:09:34 +0800

    gallery(Timeline): migrate TimelineShowCase from release/5.0 to Avalonia 12

---
+++ controlgallery/AtomUIGallery/ShowCases/ViewModels/DataDisplay/TimelineViewModel.cs
@@ -0,0 +1,18 @@
+using AtomUI.Controls;
+using ReactiveUI;
+
+namespace AtomUIGallery.ShowCases.ViewModels;
+
+public class TimelineViewModel : ReactiveObject, IRoutableViewModel
+{
+    public static EntityKey ID = "Timeline";
+
+    public IScreen HostScreen { get; }
+
+    public string UrlPathSegment { get; } = ID.ToString();
+
+    public TimelineViewModel(IScreen screen)
+    {
+        HostScreen = screen;
+    }
+}
---
+++ controlgallery/AtomUIGallery/ShowCases/Views/DataDisplay/TimelineShowCase.axaml.cs
@@ -0,0 +1,57 @@
+using System.Reactive.Disposables;
+using System.Reactive.Disposables.Fluent;
+using AtomUI.Controls;
+using AtomUIGallery.ShowCases.ViewModels;
+using ReactiveUI;
+using Avalonia.Interactivity;
+using ReactiveUI.Avalonia;
+using RadioButton = Avalonia.Controls.RadioButton;
+
+namespace AtomUIGallery.ShowCases.Views;
+
+public partial class TimelineShowCase : ReactiveUserControl<TimelineViewModel>
+{
+    public TimelineShowCase()
+    {
+        this.WhenActivated(disposables =>
+        {
+            ModeLeft.IsCheckedChanged      += ModeChecked;
+            ModeRight.IsCheckedChanged     += ModeChecked;
+            ModeAlternate.IsCheckedChanged += ModeChecked;
+            ReverseButton.Click            += ReverseButtonClick;
+
+            Disposable.Create(() =>
+            {
+                ModeLeft.IsCheckedChanged      -= ModeChecked;
+                ModeRight.IsCheckedChanged     -= ModeChecked;
+                ModeAlternate.IsCheckedChanged -= ModeChecked;
+                ReverseButton.Click            -= ReverseButtonClick;
+            }).DisposeWith(disposables);
+        });
+        InitializeComponent();
+    }
+
+    private void ReverseButtonClick(object? sender, RoutedEventArgs e)
+    {
+        ReverseTimeline.IsReverse = !ReverseTimeline.IsReverse;
+    }
+
+    private void ModeChecked(object? sender, RoutedEventArgs e)
+    {
+        if (sender is RadioButton radioButton)
+        {
+            if (radioButton == ModeLeft && ModeLeft.IsChecked == true)
+            {
+                LabelTimeline.Mode = TimeLineMode.Left;
+            }
+            else if (radioButton == ModeRight && ModeRight.IsChecked == true)
+            {
+                LabelTimeline.Mode = TimeLineMode.Right;
+            }
+            else if (radioButton == ModeAlternate && ModeAlternate.IsChecked == true)
+            {
+                LabelTimeline.Mode = TimeLineMode.Alternate;
+            }
+        }
+    }
+}
