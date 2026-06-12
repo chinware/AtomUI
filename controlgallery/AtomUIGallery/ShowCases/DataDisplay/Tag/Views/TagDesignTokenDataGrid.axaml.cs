dc76d0ed4fb64a0a2c491195703fd5535d207d4f
chinboy <qinware@163.com>
2026-06-12 17:45:50 +0800

    feat(Tag): implement TagApiDataGrid and TagDesignTokenDataGrid with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/DataDisplay/Tag/Views/TagDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Tag;
+
+public partial class TagDesignTokenDataGrid : GalleryReactiveUserControl<TagViewModel>
+{
+    public TagDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is TagViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
