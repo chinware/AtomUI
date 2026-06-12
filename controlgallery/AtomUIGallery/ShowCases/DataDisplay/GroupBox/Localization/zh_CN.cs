c83a573051203b9a720f79f18dee5019dec371c3
chinboy <qinware@163.com>
2026-06-12 16:25:56 +0800

    feat(GroupBox): implement GroupBoxApiDataGrid and GroupBoxDesignTokenDataGrid with localization support

--- controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Localization/zh_CN.cs
+++ controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Localization/zh_CN.cs
@@ -16,6 +16,41 @@ internal partial class zh_CN : LanguageProvider
     public const string HeaderIconDescription = "GroupBox 标题支持指定图标。";
     public const string P2HeaderTitleTitleInfo = "标题信息";
     public const string P2TextContentOfGroupBox = "分组框内容";
+    public const string ScenarioExamples = "示例";
+    public const string ScenarioApi = "API";
+    public const string ScenarioDesignToken = "设计 Token";
+    public const string PageSubtitle = "用带标题的边框容器组织相关内容。";
+    public const string PageDescription =
+        "GroupBox 通过带边框的容器包裹相关内容，并支持配置标题、位置、图标和字体样式。";
+    public const string ComponentCategory = "数据展示";
+    public const string ComponentStatusStable = "稳定";
+    public const string InfoNamespaceLabel = "命名空间";
+    public const string InfoPackageLabel = "包";
+    public const string InfoBaseClassLabel = "基类";
+    public const string ApiColumnProperty = "属性";
+    public const string ApiColumnDescription = "说明";
+    public const string ApiColumnType = "类型";
+    public const string ApiColumnDefault = "默认值";
+    public const string ApiPropertyHeaderTitle = "显示在分组标题区域的文本。";
+    public const string ApiPropertyHeaderTitleColor = "标题文本使用的画刷。";
+    public const string ApiPropertyHeaderIcon = "显示在标题文本前的可选图标。";
+    public const string ApiPropertyHeaderTitlePosition = "控制标题左对齐、居中或右对齐。";
+    public const string ApiPropertyHeaderFontSize = "标题文本字号。";
+    public const string ApiPropertyHeaderFontStyle = "标题文本字体样式。";
+    public const string ApiPropertyHeaderFontWeight = "标题文本字重。";
+    public const string TokenColumnToken = "Token";
+    public const string TokenColumnDescription = "说明";
+    public const string TokenColumnScope = "作用域";
+    public const string TokenColumnStatus = "状态";
+    public const string TokenNameContentPadding = "内容区域内间距。";
+    public const string TokenNameHeaderContainerMargin = "标题容器外间距。";
+    public const string TokenNameHeaderContentPadding = "标题内容区域内间距。";
+    public const string TokenNameHeaderIconMargin = "标题图标外间距。";
+    public const string TokenNameOrientationMarginPercent = "标题定位时两侧偏移比例。";
+    public const string TokenNameTextPaddingInline = "以 em 为单位的文本横向内间距。";
+    public const string TokenNameVerticalMarginInline = "纵向分割线的横向外间距。";
+    public const string TokenScopeComponent = "组件";
+    public const string TokenStatusStable = "稳定";
 
     protected override Type GetResourceKindType() => typeof(GroupBoxShowCaseLangResourceKind);
 }
---
+++ tests/AtomUIGallery.Tests/ShowCases/GroupBoxShowCasePageTests.cs
@@ -0,0 +1,211 @@
+using System;
+using System.IO;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace AtomUIGallery.Tests.ShowCases;
+
+public class GroupBoxShowCasePageTests
+{
+    [Fact]
+    public void GroupBox_ShowCase_Uses_Document_Layout_With_Examples_And_Tables()
+    {
+        var source = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxShowCase.axaml");
+
+        source.ShouldContain("GroupBoxShowCaseLangResource PageSubtitle");
+        source.ShouldContain("GroupBoxShowCaseLangResource PageDescription");
+        source.ShouldContain("GroupBoxShowCaseLangResource InfoNamespaceLabel");
+        source.ShouldContain("GroupBoxShowCaseLangResource InfoPackageLabel");
+        source.ShouldContain("GroupBoxShowCaseLangResource InfoBaseClassLabel");
+        source.ShouldContain("GroupBoxShowCaseLangResource ComponentCategory");
+        source.ShouldContain("GroupBoxShowCaseLangResource ComponentStatusStable");
+        source.ShouldContain("GroupBoxShowCaseLangResource ScenarioExamples");
+        source.ShouldContain("GroupBoxShowCaseLangResource ScenarioApi");
+        source.ShouldContain("GroupBoxShowCaseLangResource ScenarioDesignToken");
+        source.ShouldContain("Tag=\"Examples\"");
+        source.ShouldContain("Tag=\"Api\"");
+        source.ShouldContain("Tag=\"DesignToken\"");
+        source.ShouldContain("<gallery:GalleryStickyTabsHost");
+        source.ShouldContain("StickyContentPadding=\"28,0,28,0\"");
+        source.ShouldContain("<atom:TabStrip Name=\"ScenarioTabs\"");
+        source.ShouldContain("<ContentControl Name=\"ScenarioContentHost\">");
+        source.ShouldContain("Name=\"ExamplesContent\"");
+        source.ShouldContain("IsScrollEnabled=\"False\"");
+        source.ShouldContain("ContentMargin=\"28,10,28,0\"");
+        source.ShouldContain("Selector=\"atom|TextBlock.info-label\"");
+        source.ShouldContain("Selector=\"atom|TextBlock.info-value\"");
+        CountOccurrences(source, "Classes=\"info-label\"").ShouldBe(3);
+        CountOccurrences(source, "Classes=\"info-value\"").ShouldBe(3);
+        source.ShouldContain("LineHeight=\"22\"");
+        source.ShouldContain("Text=\"{gallery:GroupBoxShowCaseLangResource PageDescription}\"");
+        source.ShouldContain("HorizontalAlignment=\"Left\"");
+        source.ShouldContain("<gallery:ShowCaseItem");
+        source.ShouldContain("GroupBoxShowCaseLangResource BasicTitle");
+        source.ShouldContain("GroupBoxShowCaseLangResource HeaderPositionTitle");
+        source.ShouldContain("GroupBoxShowCaseLangResource HeaderStyleTitle");
+        source.ShouldContain("GroupBoxShowCaseLangResource HeaderIconTitle");
+        source.ShouldNotContain("<atom:TabControl");
+        source.ShouldNotContain("<atom:DataGrid");
+        source.ShouldNotContain(">Gallery<");
+    }
+
+    [Fact]
+    public void GroupBox_ShowCase_Lazy_Loads_Api_And_DesignToken_DataGrids()
+    {
+        var pageSource       = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxShowCase.axaml");
+        var codeBehindSource = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxShowCase.axaml.cs");
+        var apiSource        = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxApiDataGrid.axaml");
+        var apiCodeSource    = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxApiDataGrid.axaml.cs");
+        var tokenSource      = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxDesignTokenDataGrid.axaml");
+        var tokenCodeSource  = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxDesignTokenDataGrid.axaml.cs");
+
+        pageSource.ShouldContain("<atom:TabStrip Name=\"ScenarioTabs\"");
+        pageSource.ShouldContain("Name=\"ScenarioContentHost\"");
+        pageSource.ShouldContain("Tag=\"Api\"");
+        pageSource.ShouldContain("Tag=\"DesignToken\"");
+        pageSource.ShouldNotContain("ItemsSource=\"{Binding ApiRows}\"");
+        pageSource.ShouldNotContain("ItemsSource=\"{Binding DesignTokenRows}\"");
+
+        codeBehindSource.ShouldContain("ScenarioTabs.SelectionChanged += HandleScenarioSelectionChanged");
+        codeBehindSource.ShouldContain("EnsureSelectedScenarioContent");
+        codeBehindSource.ShouldContain("ScenarioContentHost.Content = content");
+        codeBehindSource.ShouldContain("ExamplesContent");
+        codeBehindSource.ShouldContain("new GroupBoxApiDataGrid()");
+        codeBehindSource.ShouldContain("new GroupBoxDesignTokenDataGrid()");
+
+        apiSource.ShouldContain("<atom:DataGrid");
+        apiSource.ShouldContain("x:DataType=\"viewModels:GroupBoxApiRow\"");
+        apiSource.ShouldContain("GridLinesVisibility=\"Horizontal\"");
+        apiSource.ShouldContain("IsFrameBorderVisible=\"True\"");
+        apiSource.ShouldContain("PaginationVisibility=\"None\"");
+        apiSource.ShouldContain("LeftFrozenColumnCount=\"1\"");
+        apiSource.ShouldContain("Margin=\"28,10,28,0\"");
+        apiSource.ShouldContain("HorizontalAlignment=\"Stretch\"");
+        apiSource.ShouldContain("VerticalAlignment=\"Top\"");
+        apiSource.ShouldContain("GroupBoxShowCaseLangResource ApiColumnProperty");
+        apiSource.ShouldContain("GroupBoxShowCaseLangResource ApiColumnDescription");
+        apiSource.ShouldContain("GroupBoxShowCaseLangResource ApiColumnType");
+        apiSource.ShouldContain("GroupBoxShowCaseLangResource ApiColumnDefault");
+        apiSource.ShouldContain("MinWidth=\"520\"");
+        apiSource.ShouldContain("Width=\"*\"");
+        apiCodeSource.ShouldContain("viewModel.EnsureApiRows()");
+        apiCodeSource.ShouldContain("ApiDataGrid.ItemsSource = viewModel.ApiRows");
+
+        tokenSource.ShouldContain("<atom:DataGrid");
+        tokenSource.ShouldContain("x:DataType=\"viewModels:GroupBoxDesignTokenRow\"");
+        tokenSource.ShouldContain("GridLinesVisibility=\"Horizontal\"");
+        tokenSource.ShouldContain("IsFrameBorderVisible=\"True\"");
+        tokenSource.ShouldContain("PaginationVisibility=\"None\"");
+        tokenSource.ShouldContain("LeftFrozenColumnCount=\"1\"");
+        tokenSource.ShouldContain("Margin=\"28,10,28,0\"");
+        tokenSource.ShouldContain("HorizontalAlignment=\"Stretch\"");
+        tokenSource.ShouldContain("VerticalAlignment=\"Top\"");
+        tokenSource.ShouldContain("GroupBoxShowCaseLangResource TokenColumnToken");
+        tokenSource.ShouldContain("GroupBoxShowCaseLangResource TokenColumnDescription");
+        tokenSource.ShouldContain("GroupBoxShowCaseLangResource TokenColumnScope");
+        tokenSource.ShouldContain("GroupBoxShowCaseLangResource TokenColumnStatus");
+        tokenSource.ShouldContain("MinWidth=\"520\"");
+        tokenSource.ShouldContain("Width=\"*\"");
+        tokenCodeSource.ShouldContain("viewModel.EnsureDesignTokenRows()");
+        tokenCodeSource.ShouldContain("DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows");
+    }
+
+    [Fact]
+    public void GroupBox_ShowCase_Localization_Includes_Page_And_Api_Copy()
+    {
+        var en   = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Localization/en_US.cs");
+        var zhCn = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Localization/zh_CN.cs");
+        var zhTw = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Localization/zh_TW.cs");
+
+        foreach (var source in new[] { en, zhCn, zhTw })
+        {
+            source.ShouldContain("ScenarioExamples");
+            source.ShouldContain("ScenarioApi");
+            source.ShouldContain("ScenarioDesignToken");
+            source.ShouldContain("PageSubtitle");
+            source.ShouldContain("InfoNamespaceLabel");
+            source.ShouldContain("ApiPropertyHeaderTitle");
+            source.ShouldContain("ApiPropertyHeaderTitlePosition");
+            source.ShouldContain("ApiPropertyHeaderIcon");
+            source.ShouldContain("ApiPropertyHeaderFontWeight");
+            source.ShouldContain("TokenNameContentPadding");
+        }
+    }
+
+    [Fact]
+    public void GroupBox_ShowCase_Examples_Match_Approved_Control_Demo_Content()
+    {
+        var source   = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/DataDisplay/GroupBox/Views/GroupBoxShowCase.axaml");
+        var approved = ReadRepoFile("tests/AtomUIGallery.Tests/ShowCases/GroupBoxShowCaseExamples.snapshot");
+
+        NormalizeMarkup(ExtractGroupBoxExampleItems(source))
+            .ShouldBe(NormalizeMarkup(approved));
+    }
+
+    private static string ExtractGroupBoxExampleItems(string source)
+    {
+        const string firstItemMarker  = "<gallery:ShowCaseItem";
+        const string panelCloseMarker = "</gallery:ShowCasePanel>";
+
+        var firstItemStart = source.IndexOf(firstItemMarker, StringComparison.Ordinal);
+        firstItemStart.ShouldBeGreaterThanOrEqualTo(0);
+
+        var panelCloseStart = source.IndexOf(panelCloseMarker, firstItemStart, StringComparison.Ordinal);
+        panelCloseStart.ShouldBeGreaterThan(firstItemStart);
+
+        return source[firstItemStart..panelCloseStart];
+    }
+
+    private static string NormalizeMarkup(string source)
+    {
+        return string.Join(
+            "\n",
+            source
+                .Replace("\r\n", "\n")
+                .Split('\n')
+                .Select(line => line.Trim())
+                .Where(line => line.Length > 0));
+    }
+
+    private static int CountOccurrences(string source, string value)
+    {
+        var count      = 0;
+        var startIndex = 0;
+        while (true)
+        {
+            var matchIndex = source.IndexOf(value, startIndex, StringComparison.Ordinal);
+            if (matchIndex < 0)
+            {
+                return count;
+            }
+
+            count++;
+            startIndex = matchIndex + value.Length;
+        }
+    }
+
+    private static string ReadRepoFile(string relativePath)
+    {
+        var path = GetRepoFile(relativePath);
+        File.Exists(path).ShouldBeTrue($"Expected repository file to exist: {relativePath}");
+        return File.ReadAllText(path);
+    }
+
+    private static string GetRepoFile(string relativePath)
+    {
+        var directory = new DirectoryInfo(AppContext.BaseDirectory);
+        while (directory is not null)
+        {
+            var candidate = Path.Combine(directory.FullName, relativePath);
+            if (File.Exists(candidate))
+            {
+                return candidate;
+            }
+
+            directory = directory.Parent;
+        }
+
+        return Path.Combine(AppContext.BaseDirectory, relativePath);
+    }
+}
