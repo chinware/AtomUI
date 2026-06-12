6970876a6be6c3e9be1cde838b2e2aa5abb423c9
chinboy <qinware@163.com>
2026-06-12 21:13:33 +0800

    feat(Result): add ResultApiDataGrid and ResultDesignTokenDataGrid components with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Result;
+
+public partial class ResultDesignTokenDataGrid : GalleryReactiveUserControl<ResultViewModel>
+{
+    public ResultDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is ResultViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
---
+++ tests/AtomUIGallery.Tests/ShowCases/ResultShowCasePageTests.cs
@@ -0,0 +1,218 @@
+using System;
+using System.IO;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace AtomUIGallery.Tests.ShowCases;
+
+public class ResultShowCasePageTests
+{
+    [Fact]
+    public void Result_ShowCase_Uses_Document_Layout_With_Examples_And_Tables()
+    {
+        var source = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultShowCase.axaml");
+
+        source.ShouldContain("ResultShowCaseLangResource PageSubtitle");
+        source.ShouldContain("ResultShowCaseLangResource PageDescription");
+        source.ShouldContain("ResultShowCaseLangResource InfoNamespaceLabel");
+        source.ShouldContain("ResultShowCaseLangResource InfoPackageLabel");
+        source.ShouldContain("ResultShowCaseLangResource InfoBaseClassLabel");
+        source.ShouldContain("ResultShowCaseLangResource ComponentCategory");
+        source.ShouldContain("ResultShowCaseLangResource ComponentStatusStable");
+        source.ShouldContain("ResultShowCaseLangResource ScenarioExamples");
+        source.ShouldContain("ResultShowCaseLangResource ScenarioApi");
+        source.ShouldContain("ResultShowCaseLangResource ScenarioDesignToken");
+        source.ShouldContain("Tag=\"Examples\"");
+        source.ShouldContain("Tag=\"Api\"");
+        source.ShouldContain("Tag=\"DesignToken\"");
+        source.ShouldContain("<gallery:GalleryStickyTabsHost");
+        source.ShouldContain("StickyContentPadding=\"28,0,28,0\"");
+        source.ShouldContain("<atom:TabStrip Name=\"ScenarioTabs\"");
+        source.ShouldContain("<ContentControl Name=\"ScenarioContentHost\">");
+        source.ShouldContain("Name=\"ExamplesContent\"");
+        source.ShouldContain("IsScrollEnabled=\"False\"");
+        source.ShouldContain("ContentMargin=\"28,10,28,28\"");
+        source.ShouldContain("Selector=\"atom|TextBlock.info-label\"");
+        source.ShouldContain("Selector=\"atom|TextBlock.info-value\"");
+        CountOccurrences(source, "Classes=\"info-label\"").ShouldBe(3);
+        CountOccurrences(source, "Classes=\"info-value\"").ShouldBe(3);
+        source.ShouldContain("LineHeight=\"22\"");
+        source.ShouldContain("Text=\"{gallery:ResultShowCaseLangResource PageDescription}\"");
+        source.ShouldContain("HorizontalAlignment=\"Left\"");
+        source.ShouldContain("<gallery:ShowCaseItem");
+        source.ShouldContain("ResultShowCaseLangResource SuccessTitle");
+        source.ShouldContain("ResultShowCaseLangResource InfoTitle");
+        source.ShouldContain("ResultShowCaseLangResource WarningTitle");
+        source.ShouldContain("ResultShowCaseLangResource ForbiddenTitle");
+        source.ShouldContain("ResultShowCaseLangResource NotFoundTitle");
+        source.ShouldContain("ResultShowCaseLangResource ServerErrorTitle");
+        source.ShouldContain("ResultShowCaseLangResource ErrorTitle");
+        source.ShouldContain("ResultShowCaseLangResource CustomIconTitle");
+        source.ShouldNotContain("<atom:TabControl");
+        source.ShouldNotContain("<atom:DataGrid");
+        source.ShouldNotContain(">Gallery<");
+    }
+
+    [Fact]
+    public void Result_ShowCase_Lazy_Loads_Api_And_DesignToken_DataGrids()
+    {
+        var pageSource       = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultShowCase.axaml");
+        var codeBehindSource = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultShowCase.axaml.cs");
+        var apiSource        = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultApiDataGrid.axaml");
+        var apiCodeSource    = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultApiDataGrid.axaml.cs");
+        var tokenSource      = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultDesignTokenDataGrid.axaml");
+        var tokenCodeSource  = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultDesignTokenDataGrid.axaml.cs");
+
+        pageSource.ShouldContain("<atom:TabStrip Name=\"ScenarioTabs\"");
+        pageSource.ShouldContain("Name=\"ScenarioContentHost\"");
+        pageSource.ShouldContain("Tag=\"Api\"");
+        pageSource.ShouldContain("Tag=\"DesignToken\"");
+        pageSource.ShouldNotContain("ItemsSource=\"{Binding ApiRows}\"");
+        pageSource.ShouldNotContain("ItemsSource=\"{Binding DesignTokenRows}\"");
+
+        codeBehindSource.ShouldContain("ScenarioTabs.SelectionChanged += HandleScenarioSelectionChanged");
+        codeBehindSource.ShouldContain("EnsureSelectedScenarioContent");
+        codeBehindSource.ShouldContain("ScenarioContentHost.Content = content");
+        codeBehindSource.ShouldContain("ExamplesContent");
+        codeBehindSource.ShouldContain("new ResultApiDataGrid()");
+        codeBehindSource.ShouldContain("new ResultDesignTokenDataGrid()");
+
+        apiSource.ShouldContain("<atom:DataGrid");
+        apiSource.ShouldContain("x:DataType=\"viewModels:ResultApiRow\"");
+        apiSource.ShouldContain("GridLinesVisibility=\"Horizontal\"");
+        apiSource.ShouldContain("IsFrameBorderVisible=\"True\"");
+        apiSource.ShouldContain("PaginationVisibility=\"None\"");
+        apiSource.ShouldContain("LeftFrozenColumnCount=\"1\"");
+        apiSource.ShouldContain("Margin=\"28,10,28,28\"");
+        apiSource.ShouldContain("HorizontalAlignment=\"Stretch\"");
+        apiSource.ShouldContain("VerticalAlignment=\"Top\"");
+        apiSource.ShouldContain("ResultShowCaseLangResource ApiColumnProperty");
+        apiSource.ShouldContain("ResultShowCaseLangResource ApiColumnDescription");
+        apiSource.ShouldContain("ResultShowCaseLangResource ApiColumnType");
+        apiSource.ShouldContain("ResultShowCaseLangResource ApiColumnDefault");
+        apiSource.ShouldContain("MinWidth=\"520\"");
+        apiSource.ShouldContain("Width=\"*\"");
+        apiCodeSource.ShouldContain("viewModel.EnsureApiRows()");
+        apiCodeSource.ShouldContain("ApiDataGrid.ItemsSource = viewModel.ApiRows");
+
+        tokenSource.ShouldContain("<atom:DataGrid");
+        tokenSource.ShouldContain("x:DataType=\"viewModels:ResultDesignTokenRow\"");
+        tokenSource.ShouldContain("GridLinesVisibility=\"Horizontal\"");
+        tokenSource.ShouldContain("IsFrameBorderVisible=\"True\"");
+        tokenSource.ShouldContain("PaginationVisibility=\"None\"");
+        tokenSource.ShouldContain("LeftFrozenColumnCount=\"1\"");
+        tokenSource.ShouldContain("Margin=\"28,10,28,28\"");
+        tokenSource.ShouldContain("HorizontalAlignment=\"Stretch\"");
+        tokenSource.ShouldContain("VerticalAlignment=\"Top\"");
+        tokenSource.ShouldContain("ResultShowCaseLangResource TokenColumnToken");
+        tokenSource.ShouldContain("ResultShowCaseLangResource TokenColumnDescription");
+        tokenSource.ShouldContain("ResultShowCaseLangResource TokenColumnScope");
+        tokenSource.ShouldContain("ResultShowCaseLangResource TokenColumnStatus");
+        tokenSource.ShouldContain("MinWidth=\"520\"");
+        tokenSource.ShouldContain("Width=\"*\"");
+        tokenCodeSource.ShouldContain("viewModel.EnsureDesignTokenRows()");
+        tokenCodeSource.ShouldContain("DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows");
+    }
+
+    [Fact]
+    public void Result_ShowCase_Localization_Includes_Page_And_Api_Copy()
+    {
+        var en   = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Localization/en_US.cs");
+        var zhCn = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Localization/zh_CN.cs");
+        var zhTw = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Localization/zh_TW.cs");
+
+        foreach (var source in new[] { en, zhCn, zhTw })
+        {
+            source.ShouldContain("ScenarioExamples");
+            source.ShouldContain("ScenarioApi");
+            source.ShouldContain("ScenarioDesignToken");
+            source.ShouldContain("PageSubtitle");
+            source.ShouldContain("InfoNamespaceLabel");
+            source.ShouldContain("ApiPropertyStatus");
+            source.ShouldContain("ApiPropertyIcon");
+            source.ShouldContain("ApiPropertyHeader");
+            source.ShouldContain("ApiPropertySubHeader");
+            source.ShouldContain("ApiPropertyExtra");
+            source.ShouldContain("TokenNameHeaderFontSize");
+            source.ShouldContain("TokenNameSubHeaderFontSize");
+            source.ShouldContain("TokenNameIconSize");
+        }
+    }
+
+    [Fact]
+    public void Result_ShowCase_Examples_Match_Approved_Control_Demo_Content()
+    {
+        var source   = ReadRepoFile("controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultShowCase.axaml");
+        var approved = ReadRepoFile("tests/AtomUIGallery.Tests/ShowCases/ResultShowCaseExamples.snapshot");
+
+        NormalizeMarkup(ExtractResultExampleItems(source))
+            .ShouldBe(NormalizeMarkup(approved));
+    }
+
+    private static string ExtractResultExampleItems(string source)
+    {
+        const string firstItemMarker  = "<gallery:ShowCaseItem";
+        const string panelCloseMarker = "</gallery:ShowCasePanel>";
+
+        var firstItemStart = source.IndexOf(firstItemMarker, StringComparison.Ordinal);
+        firstItemStart.ShouldBeGreaterThanOrEqualTo(0);
+
+        var panelCloseStart = source.IndexOf(panelCloseMarker, firstItemStart, StringComparison.Ordinal);
+        panelCloseStart.ShouldBeGreaterThan(firstItemStart);
+
+        return source[firstItemStart..panelCloseStart];
+    }
+
+    private static string NormalizeMarkup(string source)
+    {
+        return string.Join(
+            "\n",
+            source
+                .Replace("\r\n", "\n")
+                .Split('\n')
+                .Select(line => line.Trim())
+                .Where(line => line.Length > 0));
+    }
+
+    private static int CountOccurrences(string source, string value)
+    {
+        var count      = 0;
+        var startIndex = 0;
+        while (true)
+        {
+            var matchIndex = source.IndexOf(value, startIndex, StringComparison.Ordinal);
+            if (matchIndex < 0)
+            {
+                return count;
+            }
+
+            count++;
+            startIndex = matchIndex + value.Length;
+        }
+    }
+
+    private static string ReadRepoFile(string relativePath)
+    {
+        var path = GetRepoFile(relativePath);
+        File.Exists(path).ShouldBeTrue($"Expected repository file to exist: {relativePath}");
+        return File.ReadAllText(path);
+    }
+
+    private static string GetRepoFile(string relativePath)
+    {
+        var directory = new DirectoryInfo(AppContext.BaseDirectory);
+        while (directory is not null)
+        {
+            var candidate = Path.Combine(directory.FullName, relativePath);
+            if (File.Exists(candidate))
+            {
+                return candidate;
+            }
+
+            directory = directory.Parent;
+        }
+
+        return Path.Combine(AppContext.BaseDirectory, relativePath);
+    }
+}
