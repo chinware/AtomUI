51e69603aee77fb1a916f68a1a2c268745fae0a6
chinboy <qinware@163.com>
2026-06-12 22:02:51 +0800

    feat(Skeleton): add SkeletonApiDataGrid and SkeletonDesignTokenDataGrid components with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/Feedback/Skeleton/Views/SkeletonApiDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Skeleton;
+
+public partial class SkeletonApiDataGrid : GalleryReactiveUserControl<SkeletonViewModel>
+{
+    public SkeletonApiDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is SkeletonViewModel viewModel)
+        {
+            viewModel.EnsureApiRows();
+            ApiDataGrid.ItemsSource = viewModel.ApiRows;
+        }
+        else
+        {
+            ApiDataGrid.ItemsSource = null;
+        }
+    }
+}
---
+++ controlgallery/AtomUIGallery/ShowCases/Feedback/Skeleton/Views/SkeletonDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Skeleton;
+
+public partial class SkeletonDesignTokenDataGrid : GalleryReactiveUserControl<SkeletonViewModel>
+{
+    public SkeletonDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is SkeletonViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
