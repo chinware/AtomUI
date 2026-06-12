7e88a7efd49ebf4e139fa83711d4b9833a1037a3
chinboy <qinware@163.com>
2026-06-12 22:24:55 +0800

    feat(Spin): add SpinApiDataGrid and SpinDesignTokenDataGrid components with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/Feedback/Spin/Views/SpinApiDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Spin;
+
+public partial class SpinApiDataGrid : GalleryReactiveUserControl<SpinViewModel>
+{
+    public SpinApiDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is SpinViewModel viewModel)
+        {
+            viewModel.EnsureApiRows();
+            ApiDataGrid.ItemsSource = viewModel.ApiRows;
+        }
+        else
+        {
+            ApiDataGrid.ItemsSource = null;
+        }
+    }
+}
