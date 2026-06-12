991f98625398d95a0608e9494b5f7e958e4ccea9
chinboy <qinware@163.com>
2026-06-12 18:35:51 +0800

    feat(Timeline): add API and token reference tables

---
+++ controlgallery/AtomUIGallery/ShowCases/DataDisplay/Timeline/Views/TimelineDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Timeline;
+
+public partial class TimelineDesignTokenDataGrid : GalleryReactiveUserControl<TimelineViewModel>
+{
+    public TimelineDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is TimelineViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
