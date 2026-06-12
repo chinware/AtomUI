4322d1068ce335be7837c26eb085ad0de549fee6
chinboy <qinware@163.com>
2026-06-12 21:13:33 +0800

    feat(Result): add ResultApiDataGrid and ResultDesignTokenDataGrid components with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/Feedback/Result/Views/ResultApiDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Result;
+
+public partial class ResultApiDataGrid : GalleryReactiveUserControl<ResultViewModel>
+{
+    public ResultApiDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is ResultViewModel viewModel)
+        {
+            viewModel.EnsureApiRows();
+            ApiDataGrid.ItemsSource = viewModel.ApiRows;
+        }
+        else
+        {
+            ApiDataGrid.ItemsSource = null;
+        }
+    }
+}
