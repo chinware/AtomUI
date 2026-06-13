b94ac79614fbb3e83b87dc94722f4b375a917ae4
chinboy <qinware@163.com>
2026-06-13 10:47:09 +0800

    feat(Carousel): add CarouselApiDataGrid and CarouselDesignTokenDataGrid components with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/DataDisplay/Carousel/Views/CarouselDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Carousel;
+
+public partial class CarouselDesignTokenDataGrid : GalleryReactiveUserControl<CarouselViewModel>
+{
+    public CarouselDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is CarouselViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
--- controlgallery/AtomUIGallery/ShowCases/DataDisplay/Carousel/Views/CarouselShowCase.axaml.cs
+++ controlgallery/AtomUIGallery/ShowCases/DataDisplay/Carousel/Views/CarouselShowCase.axaml.cs
@@ -1,21 +1,113 @@
+using System;
+using System.Collections.Generic;
 using System.Reactive.Disposables;
 using AtomUI.Controls;
 using AtomUI.Desktop.Controls;
+using Avalonia;
+using Avalonia.Controls;
 
 namespace AtomUIGallery.ShowCases.Carousel;
 
 public partial class CarouselShowCase : GalleryReactiveUserControl<CarouselViewModel>
 {
     public const string LanguageId = nameof(CarouselShowCase);
+    private const string ExamplesScenario    = "Examples";
+    private const string ApiScenario         = "Api";
+    private const string DesignTokenScenario = "DesignToken";
+
+    private readonly Dictionary<string, Control> _lazyScenarioContentCache = new(StringComparer.Ordinal);
 
     public CarouselShowCase()
     {
+        InitializeComponent();
+        ScenarioTabs.SelectionChanged += HandleScenarioSelectionChanged;
+
         this.WhenActivated(disposables =>
         {
             PositionOptionGroup.OptionCheckedChanged += HandlePositionOptionChanged;
             disposables.Add(Disposable.Create(() => PositionOptionGroup.OptionCheckedChanged -= HandlePositionOptionChanged));
         });
-        InitializeComponent();
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        EnsureSelectedScenarioContent();
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        ClearLazyScenarioContent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        ExamplesContent.DataContext = DataContext;
+        foreach (var content in _lazyScenarioContentCache.Values)
+        {
+            content.DataContext = DataContext;
+        }
+        EnsureSelectedScenarioContent();
+    }
+
+    private void HandleScenarioSelectionChanged(object? sender, SelectionChangedEventArgs args)
+    {
+        EnsureSelectedScenarioContent();
+    }
+
+    private void EnsureSelectedScenarioContent()
+    {
+        if (ScenarioTabs.SelectedItem is not TabStripItem tabStripItem ||
+            tabStripItem.Tag is not string scenario)
+        {
+            return;
+        }
+
+        var content = ResolveScenarioContent(scenario);
+        if (!ReferenceEquals(ScenarioContentHost.Content, content))
+        {
+            ScenarioContentHost.Content = content;
+        }
+    }
+
+    private void ClearLazyScenarioContent()
+    {
+        if (ScenarioContentHost.Content is not null &&
+            !ReferenceEquals(ScenarioContentHost.Content, ExamplesContent))
+        {
+            ScenarioContentHost.Content = null;
+        }
+        _lazyScenarioContentCache.Clear();
+    }
+
+    private Control ResolveScenarioContent(string scenario)
+    {
+        if (scenario == ExamplesScenario)
+        {
+            ExamplesContent.DataContext = DataContext;
+            return ExamplesContent;
+        }
+
+        if (!_lazyScenarioContentCache.TryGetValue(scenario, out var content))
+        {
+            content             = CreateScenarioContent(scenario);
+            content.DataContext = DataContext;
+            _lazyScenarioContentCache.Add(scenario, content);
+        }
+
+        return content;
+    }
+
+    private static Control CreateScenarioContent(string scenario)
+    {
+        return scenario switch
+        {
+            ApiScenario         => new CarouselApiDataGrid(),
+            DesignTokenScenario => new CarouselDesignTokenDataGrid(),
+            _                   => throw new InvalidOperationException($"Unknown Carousel scenario: {scenario}")
+        };
     }
 
     public void HandlePositionOptionChanged(object? sender, OptionCheckedChangedEventArgs args)
