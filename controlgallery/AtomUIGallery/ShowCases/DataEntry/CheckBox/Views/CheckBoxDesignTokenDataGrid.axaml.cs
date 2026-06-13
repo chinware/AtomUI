6f25ab1e89062edae03b9e3a43925a7326f6c503
chinboy <qinware@163.com>
2026-06-13 15:29:59 +0800

    feat(CheckBox): add CheckBoxShowCase with API and design token support

---
+++ controlgallery/AtomUIGallery/ShowCases/DataEntry/CheckBox/Views/CheckBoxDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.CheckBox;
+
+public partial class CheckBoxDesignTokenDataGrid : GalleryReactiveUserControl<CheckBoxViewModel>
+{
+    public CheckBoxDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is CheckBoxViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
