35ee6b5d316fcb3bd3ac4e71c25ae18a4034a59a
chinboy <qinware@163.com>
2026-06-14 09:33:16 +0800

    feat(Cascader): enhance API and design token support with new language resources

---
+++ controlgallery/AtomUIGallery/ShowCases/DataEntry/Cascader/Views/CascaderApiDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Cascader;
+
+public partial class CascaderApiDataGrid : GalleryReactiveUserControl<CascaderViewModel>
+{
+    public CascaderApiDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is CascaderViewModel viewModel)
+        {
+            viewModel.EnsureApiRows();
+            ApiDataGrid.ItemsSource = viewModel.ApiRows;
+        }
+        else
+        {
+            ApiDataGrid.ItemsSource = null;
+        }
+    }
+}
