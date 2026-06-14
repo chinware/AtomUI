29825cb9888340c60692f169f319eb817117c1ee
chinboy <qinware@163.com>
2026-06-14 12:21:54 +0800

    feat(Space): add API and design token data grids with localization support

---
+++ controlgallery/AtomUIGallery/ShowCases/Layout/Space/Views/SpaceApiDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Space;
+
+public partial class SpaceApiDataGrid : GalleryReactiveUserControl<SpaceViewModel>
+{
+    public SpaceApiDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is SpaceViewModel viewModel)
+        {
+            viewModel.EnsureApiRows();
+            ApiDataGrid.ItemsSource = viewModel.ApiRows;
+        }
+        else
+        {
+            ApiDataGrid.ItemsSource = null;
+        }
+    }
+}
---
+++ controlgallery/AtomUIGallery/ShowCases/Layout/Space/Views/SpaceDesignTokenDataGrid.axaml.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AtomUIGallery.ShowCases.Space;
+
+public partial class SpaceDesignTokenDataGrid : GalleryReactiveUserControl<SpaceViewModel>
+{
+    public SpaceDesignTokenDataGrid()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is SpaceViewModel viewModel)
+        {
+            viewModel.EnsureDesignTokenRows();
+            DesignTokenDataGrid.ItemsSource = viewModel.DesignTokenRows;
+        }
+        else
+        {
+            DesignTokenDataGrid.ItemsSource = null;
+        }
+    }
+}
