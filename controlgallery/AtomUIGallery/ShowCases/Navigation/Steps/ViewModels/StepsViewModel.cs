a9f93a6fab30009efeea18a9db56bc8901b47b84
chinboy <qinware@163.com>
2026-06-14 13:23:44 +0800

    feat(Steps): implement API and design token data grids with localization support

--- controlgallery/AtomUIGallery/ShowCases/Navigation/Steps/ViewModels/StepsViewModel.cs
+++ controlgallery/AtomUIGallery/ShowCases/Navigation/Steps/ViewModels/StepsViewModel.cs
@@ -1,4 +1,10 @@
+using System.Collections.ObjectModel;
 using AtomUI.Controls;
+using AtomUI.Data;
+using AtomUI.Theme.Language;
+using AtomUIGallery.Localization;
+using Avalonia;
+using Avalonia.Threading;
 using ReactiveUI;
 
 namespace AtomUIGallery.ShowCases.Steps;
@@ -7,10 +13,12 @@ public class StepsViewModel : ReactiveObject, IRoutableViewModel
 {
     public static EntityKey ID = "Steps";
 
+    private const int InteractiveLastStepIndex = 2;
+
     public IScreen HostScreen { get; }
 
     public string UrlPathSegment { get; } = ID.ToString();
-    
+
     private int _currentStep;
 
     public int CurrentStep
@@ -20,6 +28,7 @@ public class StepsViewModel : ReactiveObject, IRoutableViewModel
         {
             this.RaiseAndSetIfChanged(ref _currentStep, value);
             PreviousButtonVisible = CurrentStep > 0;
+            RefreshInteractiveButtonText();
         }
     }
 
@@ -30,9 +39,195 @@ public class StepsViewModel : ReactiveObject, IRoutableViewModel
         get => _previousButtonVisible;
         set => this.RaiseAndSetIfChanged(ref _previousButtonVisible, value);
     }
-    
+
+    private string _nextButtonText = Lang(StepsShowCaseLangResourceKind.P2ContentNext);
+
+    public string NextButtonText
+    {
+        get => _nextButtonText;
+        set => this.RaiseAndSetIfChanged(ref _nextButtonText, value);
+    }
+
+    private ObservableCollection<StepsApiRow>? _apiRows;
+    private ObservableCollection<StepsDesignTokenRow>? _designTokenRows;
+
+    public ObservableCollection<StepsApiRow>? ApiRows
+    {
+        get => _apiRows;
+        private set => this.RaiseAndSetIfChanged(ref _apiRows, value);
+    }
+
+    public ObservableCollection<StepsDesignTokenRow>? DesignTokenRows
+    {
+        get => _designTokenRows;
+        private set => this.RaiseAndSetIfChanged(ref _designTokenRows, value);
+    }
+
     public StepsViewModel(IScreen screen)
     {
         HostScreen = screen;
     }
+
+    public void ResetInteractiveStep()
+    {
+        CurrentStep            = 0;
+        PreviousButtonVisible = false;
+        RefreshInteractiveButtonText();
+    }
+
+    public void MoveToNextInteractiveStep()
+    {
+        if (CurrentStep < InteractiveLastStepIndex)
+        {
+            CurrentStep++;
+        }
+
+        RefreshInteractiveButtonText();
+    }
+
+    public void MoveToPreviousInteractiveStep()
+    {
+        if (CurrentStep > 0)
+        {
+            CurrentStep--;
+        }
+
+        RefreshInteractiveButtonText();
+    }
+
+    public void RefreshInteractiveButtonText()
+    {
+        NextButtonText = CurrentStep == InteractiveLastStepIndex
+            ? Lang(StepsShowCaseLangResourceKind.P2ContentDone)
+            : Lang(StepsShowCaseLangResourceKind.P2ContentNext);
+    }
+
+    public void EnsureApiRows()
+    {
+        if (ApiRows is not null)
+        {
+            return;
+        }
+
+        ApiRows =
+        [
+            new StepsApiRow("Steps.CurrentStep", Lang(StepsShowCaseLangResourceKind.ApiPropertyCurrentStep), "int", "cyan", "0"),
+            new StepsApiRow("Steps.InitialStep", Lang(StepsShowCaseLangResourceKind.ApiPropertyInitialStep), "int", "cyan", "-1"),
+            new StepsApiRow("Steps.ProgressValue", Lang(StepsShowCaseLangResourceKind.ApiPropertyProgressValue), "double", "cyan", "0"),
+            new StepsApiRow("Steps.CurrentStepStatus", Lang(StepsShowCaseLangResourceKind.ApiPropertyCurrentStepStatus), "StepsItemStatus", "blue", "Process"),
+            new StepsApiRow("Steps.Orientation", Lang(StepsShowCaseLangResourceKind.ApiPropertyOrientation), "Orientation", "blue", "Horizontal"),
+            new StepsApiRow("Steps.LabelPlacement", Lang(StepsShowCaseLangResourceKind.ApiPropertyLabelPlacement), "Orientation", "blue", "Horizontal"),
+            new StepsApiRow("Steps.SizeType", Lang(StepsShowCaseLangResourceKind.ApiPropertySizeType), "SizeType", "blue", "Middle"),
+            new StepsApiRow("Steps.ItemIndicatorType", Lang(StepsShowCaseLangResourceKind.ApiPropertyItemIndicatorType), "StepsItemIndicatorType", "blue", "Default"),
+            new StepsApiRow("Steps.Style", Lang(StepsShowCaseLangResourceKind.ApiPropertyStyle), "StepsStyle", "blue", "Default"),
+            new StepsApiRow("Steps.IsItemClickable", Lang(StepsShowCaseLangResourceKind.ApiPropertyIsItemClickable), "bool", "purple", "false"),
+            new StepsApiRow("Steps.IsShowItemProgress", Lang(StepsShowCaseLangResourceKind.ApiPropertyIsShowItemProgress), "bool", "purple", "false"),
+            new StepsApiRow("Steps.ContentTemplate", Lang(StepsShowCaseLangResourceKind.ApiPropertyContentTemplate), "IDataTemplate?", "cyan", "null"),
+            new StepsApiRow("Steps.CurrentContent", Lang(StepsShowCaseLangResourceKind.ApiPropertyCurrentContent), "object?", "cyan", "read-only"),
+            new StepsApiRow("StepsItem.SubHeader", Lang(StepsShowCaseLangResourceKind.ApiPropertyStepsItemSubHeader), "object?", "cyan", "null"),
+            new StepsApiRow("StepsItem.Description", Lang(StepsShowCaseLangResourceKind.ApiPropertyStepsItemDescription), "object?", "cyan", "null"),
+            new StepsApiRow("StepsItem.Icon", Lang(StepsShowCaseLangResourceKind.ApiPropertyStepsItemIcon), "PathIcon?", "cyan", "null"),
+            new StepsApiRow("StepsItem.Status", Lang(StepsShowCaseLangResourceKind.ApiPropertyStepsItemStatus), "StepsItemStatus", "blue", "Process")
+        ];
+    }
+
+    public void EnsureDesignTokenRows()
+    {
+        if (DesignTokenRows is not null)
+        {
+            return;
+        }
+
+        DesignTokenRows =
+        [
+            new StepsDesignTokenRow("DescriptionMaxWidth", Lang(StepsShowCaseLangResourceKind.TokenNameDescriptionMaxWidth), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("IconSize", Lang(StepsShowCaseLangResourceKind.TokenNameIconSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("IconFontSize", Lang(StepsShowCaseLangResourceKind.TokenNameIconFontSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("IconSizeSM", Lang(StepsShowCaseLangResourceKind.TokenNameIconSizeSM), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("DotSize", Lang(StepsShowCaseLangResourceKind.TokenNameDotSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("DotCurrentSize", Lang(StepsShowCaseLangResourceKind.TokenNameDotCurrentSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("DotLineThickness", Lang(StepsShowCaseLangResourceKind.TokenNameDotLineThickness), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("HorizontalHeaderMargin", Lang(StepsShowCaseLangResourceKind.TokenNameHorizontalHeaderMargin), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("VerticalItemSpacing", Lang(StepsShowCaseLangResourceKind.TokenNameVerticalItemSpacing), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("VerticalDescriptionPadding", Lang(StepsShowCaseLangResourceKind.TokenNameVerticalDescriptionPadding), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("StepsNavActiveColor", Lang(StepsShowCaseLangResourceKind.TokenNameStepsNavActiveColor), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("StepsProgressSize", Lang(StepsShowCaseLangResourceKind.TokenNameStepsProgressSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("InlineDotSize", Lang(StepsShowCaseLangResourceKind.TokenNameInlineDotSize), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("InlineItemPadding", Lang(StepsShowCaseLangResourceKind.TokenNameInlineItemPadding), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("ProcessIconBgColor", Lang(StepsShowCaseLangResourceKind.TokenNameProcessIconBgColor), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("FinishTailColor", Lang(StepsShowCaseLangResourceKind.TokenNameFinishTailColor), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success"),
+            new StepsDesignTokenRow("ErrorIconBgColor", Lang(StepsShowCaseLangResourceKind.TokenNameErrorIconBgColor), Lang(StepsShowCaseLangResourceKind.TokenScopeComponent), "cyan", Lang(StepsShowCaseLangResourceKind.TokenStatusStable), "success")
+        ];
+    }
+
+    private static string Lang(StepsShowCaseLangResourceKind kind)
+    {
+        if (Application.Current is not null && Dispatcher.UIThread.CheckAccess())
+        {
+            return LanguageResourceBinder.GetLangResource(kind) ?? FallbackLang(kind);
+        }
+
+        return FallbackLang(kind);
+    }
+
+    private static string FallbackLang(StepsShowCaseLangResourceKind kind)
+    {
+        return kind switch
+        {
+            StepsShowCaseLangResourceKind.P2ContentDone                         => en_US.P2ContentDone,
+            StepsShowCaseLangResourceKind.P2ContentNext                         => en_US.P2ContentNext,
+            StepsShowCaseLangResourceKind.ApiPropertyCurrentStep                => en_US.ApiPropertyCurrentStep,
+            StepsShowCaseLangResourceKind.ApiPropertyInitialStep                => en_US.ApiPropertyInitialStep,
+            StepsShowCaseLangResourceKind.ApiPropertyProgressValue              => en_US.ApiPropertyProgressValue,
+            StepsShowCaseLangResourceKind.ApiPropertyCurrentStepStatus          => en_US.ApiPropertyCurrentStepStatus,
+            StepsShowCaseLangResourceKind.ApiPropertyOrientation                => en_US.ApiPropertyOrientation,
+            StepsShowCaseLangResourceKind.ApiPropertyLabelPlacement             => en_US.ApiPropertyLabelPlacement,
+            StepsShowCaseLangResourceKind.ApiPropertySizeType                   => en_US.ApiPropertySizeType,
+            StepsShowCaseLangResourceKind.ApiPropertyItemIndicatorType          => en_US.ApiPropertyItemIndicatorType,
+            StepsShowCaseLangResourceKind.ApiPropertyStyle                      => en_US.ApiPropertyStyle,
+            StepsShowCaseLangResourceKind.ApiPropertyIsItemClickable            => en_US.ApiPropertyIsItemClickable,
+            StepsShowCaseLangResourceKind.ApiPropertyIsShowItemProgress         => en_US.ApiPropertyIsShowItemProgress,
+            StepsShowCaseLangResourceKind.ApiPropertyContentTemplate            => en_US.ApiPropertyContentTemplate,
+            StepsShowCaseLangResourceKind.ApiPropertyCurrentContent             => en_US.ApiPropertyCurrentContent,
+            StepsShowCaseLangResourceKind.ApiPropertyStepsItemSubHeader         => en_US.ApiPropertyStepsItemSubHeader,
+            StepsShowCaseLangResourceKind.ApiPropertyStepsItemDescription       => en_US.ApiPropertyStepsItemDescription,
+            StepsShowCaseLangResourceKind.ApiPropertyStepsItemIcon              => en_US.ApiPropertyStepsItemIcon,
+            StepsShowCaseLangResourceKind.ApiPropertyStepsItemStatus            => en_US.ApiPropertyStepsItemStatus,
+            StepsShowCaseLangResourceKind.TokenNameDescriptionMaxWidth          => en_US.TokenNameDescriptionMaxWidth,
+            StepsShowCaseLangResourceKind.TokenNameIconSize                     => en_US.TokenNameIconSize,
+            StepsShowCaseLangResourceKind.TokenNameIconFontSize                 => en_US.TokenNameIconFontSize,
+            StepsShowCaseLangResourceKind.TokenNameIconSizeSM                   => en_US.TokenNameIconSizeSM,
+            StepsShowCaseLangResourceKind.TokenNameDotSize                      => en_US.TokenNameDotSize,
+            StepsShowCaseLangResourceKind.TokenNameDotCurrentSize               => en_US.TokenNameDotCurrentSize,
+            StepsShowCaseLangResourceKind.TokenNameDotLineThickness             => en_US.TokenNameDotLineThickness,
+            StepsShowCaseLangResourceKind.TokenNameHorizontalHeaderMargin       => en_US.TokenNameHorizontalHeaderMargin,
+            StepsShowCaseLangResourceKind.TokenNameVerticalItemSpacing          => en_US.TokenNameVerticalItemSpacing,
+            StepsShowCaseLangResourceKind.TokenNameVerticalDescriptionPadding   => en_US.TokenNameVerticalDescriptionPadding,
+            StepsShowCaseLangResourceKind.TokenNameStepsNavActiveColor          => en_US.TokenNameStepsNavActiveColor,
+            StepsShowCaseLangResourceKind.TokenNameStepsProgressSize            => en_US.TokenNameStepsProgressSize,
+            StepsShowCaseLangResourceKind.TokenNameInlineDotSize                => en_US.TokenNameInlineDotSize,
+            StepsShowCaseLangResourceKind.TokenNameInlineItemPadding            => en_US.TokenNameInlineItemPadding,
+            StepsShowCaseLangResourceKind.TokenNameProcessIconBgColor           => en_US.TokenNameProcessIconBgColor,
+            StepsShowCaseLangResourceKind.TokenNameFinishTailColor              => en_US.TokenNameFinishTailColor,
+            StepsShowCaseLangResourceKind.TokenNameErrorIconBgColor             => en_US.TokenNameErrorIconBgColor,
+            StepsShowCaseLangResourceKind.TokenScopeComponent                   => en_US.TokenScopeComponent,
+            StepsShowCaseLangResourceKind.TokenStatusStable                     => en_US.TokenStatusStable,
+            _                                                                  => kind.ToString()
+        };
+    }
 }
+
+public sealed record StepsApiRow(
+    string Property,
+    string Description,
+    string Type,
+    string TypeTagColor,
+    string Default);
+
+public sealed record StepsDesignTokenRow(
+    string Token,
+    string Description,
+    string Scope,
+    string ScopeTagColor,
+    string Status,
+    string StatusTagColor);
--- controlgallery/AtomUIGallery/ShowCases/Navigation/Steps/Views/StepsShowCase.axaml.cs
+++ controlgallery/AtomUIGallery/ShowCases/Navigation/Steps/Views/StepsShowCase.axaml.cs
@@ -1,4 +1,12 @@
+using System.Reactive.Disposables;
+using System.Reactive.Disposables.Fluent;
+using AtomUI.Controls;
+using AtomUI.Data;
+using AtomUI.Theme.Language;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ScenarioTabStripItem = AtomUI.Desktop.Controls.TabStripItem;
 
 namespace AtomUIGallery.ShowCases.Steps;
 
@@ -6,29 +14,80 @@ public partial class StepsShowCase : GalleryReactiveUserControl<StepsViewModel>
 {
     public const string LanguageId = nameof(StepsShowCase);
 
-    private const string BasicScenario        = "Basic";
-    private const string InteractiveScenario  = "Interactive";
-    private const string VerticalScenario     = "Vertical";
-    private const string DotClickableScenario = "DotClickable";
-    private const string NavigationScenario   = "Navigation";
-    private const string ProgressScenario     = "Progress";
-    private const string InlineScenario       = "Inline";
+    public static readonly StyledProperty<double[]> DashedArrayProperty =
+        AvaloniaProperty.Register<StepsShowCase, double[]>(nameof(DashedArray));
 
-    private readonly Dictionary<string, Control> _scenarioCache = new(StringComparer.Ordinal);
+    private const string ExamplesScenario    = "Examples";
+    private const string ApiScenario         = "Api";
+    private const string DesignTokenScenario = "DesignToken";
+
+    private readonly Dictionary<string, Control> _lazyScenarioContentCache = new(StringComparer.Ordinal);
+
+    public double[] DashedArray
+    {
+        get => GetValue(DashedArrayProperty);
+        set => SetValue(DashedArrayProperty, value);
+    }
 
     public StepsShowCase()
     {
         InitializeComponent();
+        DashedArray = [4d, 3d];
         ScenarioTabs.SelectionChanged += HandleScenarioSelectionChanged;
+
+        this.WhenActivated(disposables =>
+        {
+            ResetInteractiveState();
+
+            var themeManager = Application.Current?.GetThemeManager();
+            if (themeManager != null)
+            {
+                EventHandler<LanguageVariantChangedEventArgs> handler = (_, _) => RefreshInteractiveButtonText();
+                themeManager.LanguageVariantChanged += handler;
+                Disposable.Create(() => themeManager.LanguageVariantChanged -= handler)
+                          .DisposeWith(disposables);
+            }
+        });
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
         EnsureSelectedScenarioContent();
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        ClearLazyScenarioContent();
     }
 
     protected override void OnDataContextChanged(EventArgs e)
     {
         base.OnDataContextChanged(e);
-        foreach (var content in _scenarioCache.Values)
+        ExamplesContent.DataContext = DataContext;
+        foreach (var content in _lazyScenarioContentCache.Values)
         {
             content.DataContext = DataContext;
+        }
+
+        ResetInteractiveState();
+        EnsureSelectedScenarioContent();
+    }
+
+    public void HandleNextButtonClick(object? sender, RoutedEventArgs args)
+    {
+        if (DataContext is StepsViewModel viewModel)
+        {
+            viewModel.MoveToNextInteractiveStep();
+        }
+    }
+
+    public void HandlePreviousButtonClick(object? sender, RoutedEventArgs args)
+    {
+        if (DataContext is StepsViewModel viewModel)
+        {
+            viewModel.MoveToPreviousInteractiveStep();
         }
     }
 
@@ -39,37 +98,71 @@ public partial class StepsShowCase : GalleryReactiveUserControl<StepsViewModel>
 
     private void EnsureSelectedScenarioContent()
     {
-        if (ScenarioTabs.SelectedItem is not AtomUI.Desktop.Controls.TabItem tabItem ||
-            tabItem.Tag is not string scenario)
+        if (ScenarioTabs.SelectedItem is not ScenarioTabStripItem tabStripItem ||
+            tabStripItem.Tag is not string scenario)
         {
             return;
         }
 
-        if (!_scenarioCache.TryGetValue(scenario, out var content))
+        var content = ResolveScenarioContent(scenario);
+        if (!ReferenceEquals(ScenarioContentHost.Content, content))
+        {
+            ScenarioContentHost.Content = content;
+        }
+    }
+
+    private void ClearLazyScenarioContent()
+    {
+        if (ScenarioContentHost.Content is not null &&
+            !ReferenceEquals(ScenarioContentHost.Content, ExamplesContent))
+        {
+            ScenarioContentHost.Content = null;
+        }
+
+        _lazyScenarioContentCache.Clear();
+    }
+
+    private Control ResolveScenarioContent(string scenario)
+    {
+        if (scenario == ExamplesScenario)
+        {
+            ExamplesContent.DataContext = DataContext;
+            return ExamplesContent;
+        }
+
+        if (!_lazyScenarioContentCache.TryGetValue(scenario, out var content))
         {
             content             = CreateScenarioContent(scenario);
             content.DataContext = DataContext;
-            _scenarioCache.Add(scenario, content);
+            _lazyScenarioContentCache.Add(scenario, content);
         }
 
-        if (tabItem.Content != content)
-        {
-            tabItem.Content = content;
-        }
+        return content;
     }
 
     private static Control CreateScenarioContent(string scenario)
     {
         return scenario switch
         {
-            BasicScenario        => new StepsBasicShowCase(),
-            InteractiveScenario  => new StepsInteractiveShowCase(),
-            VerticalScenario     => new StepsVerticalShowCase(),
-            DotClickableScenario => new StepsDotClickableShowCase(),
-            NavigationScenario   => new StepsNavigationShowCase(),
-            ProgressScenario     => new StepsProgressShowCase(),
-            InlineScenario       => new StepsInlineShowCase(),
-            _                    => throw new InvalidOperationException($"Unknown Steps scenario: {scenario}")
+            ApiScenario         => new StepsApiDataGrid(),
+            DesignTokenScenario => new StepsDesignTokenDataGrid(),
+            _                   => throw new InvalidOperationException($"Unknown Steps scenario: {scenario}")
         };
+    }
+
+    private void ResetInteractiveState()
+    {
+        if (DataContext is StepsViewModel viewModel)
+        {
+            viewModel.ResetInteractiveStep();
+        }
+    }
+
+    private void RefreshInteractiveButtonText()
+    {
+        if (DataContext is StepsViewModel viewModel)
+        {
+            viewModel.RefreshInteractiveButtonText();
+        }
     }
 }
