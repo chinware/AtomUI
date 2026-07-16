782746ca6776893be128741119545ca47a58a7c3
chinboy <qinware@163.com>
2026-07-16 19:36:24 +0800

    refactor(Converters): rename CascaderViewItemIndicatorEnabledConverter to TreeViewItemIndicatorEnabledConverter

--- src/AtomUI.Desktop.Controls/Cascader/Converters/CascaderViewItemIndicatorEnabledConverter.cs
+++ src/AtomUI.Desktop.Controls/Cascader/Converters/CascaderViewItemIndicatorEnabledConverter.cs
@@ -3,11 +3,7 @@ using Avalonia.Data.Converters;
 
 namespace AtomUI.Desktop.Controls.Converters;
 
-<<<<<<<< HEAD:src/AtomUI.Desktop.Controls/TreeView/Converters/TreeViewItemIndicatorEnabledConverter.cs
-internal class TreeViewItemIndicatorEnabledConverter : IMultiValueConverter
-========
 internal class CascaderViewItemIndicatorEnabledConverter : IMultiValueConverter
->>>>>>>> release/6.0:src/AtomUI.Desktop.Controls/Cascader/Converters/CascaderViewItemIndicatorEnabledConverter.cs
 {
     public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
     {
