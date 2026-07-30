6565762c6ad6cac0c216fa4f3b5c96b2da7345e9
chinboy <qinware@163.com>
2026-07-30 19:27:38 +0800

    Revert "feat(Calendar): add public value types, enums, and event args"
    This reverts commit 8fb07c83541428d1bf3b546a05a3d9497a2abaa1.

--- src/AtomUI.Desktop.Controls/Calendar/CalendarEnums.cs
+++
@@ -1,5 +0,0 @@
-namespace AtomUI.Desktop.Controls;
-
-public enum CalendarMode { Month, Year, Decade }
-public enum CalendarSelectSource { Year, Month, Date, Customize }
-public enum CalendarCellType { Date, Month }
